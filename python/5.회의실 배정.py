425d63ce6ccae8372f00d2d8b966d6f94bca169c
Jihyeon <gihyeon0715@naver.com>
2021-03-04 19:31:52 +0900

    secion4

--- python/5.회의실 배정.py
+++ python/5.회의실 배정.py
@@ -1,16 +1,16 @@
 import sys
-#sys.stdin=open("input.txt", "r")
-n=int(input())
-meeting=[]
-for i in range(n):
-    s,e=map(int,input().split())
-    meeting.append((s,e))
+#sys.stdin = open("input.txt","rt")
 
-meeting.sort(key=lambda x : (x[1],x[0]))
-et=0
+n=int(input())
+a=[]
+for _ in range(n):
+    s,e=map(int,input().split())
+    a.append((s,e))
+a.sort(key=lambda x:(x[1],x[0]))
 cnt=0
-for s,e in meeting:
-    if s>=et:
-        et=e
+et=0
+for s,e in a:
+    if et<=s:
         cnt+=1
+        et=e
 print(cnt)
