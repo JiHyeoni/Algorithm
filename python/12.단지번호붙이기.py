d739de51c3cadc913d71b00a200ec135f1c5688b
Jihyeon <gihyeon0715@naver.com>
2021-03-17 16:34:20 +0900

    secion7

--- python/12.단지번호붙이기.py
+++ python/12.단지번호붙이기.py
@@ -1,19 +1,18 @@
 import sys
-from collections import  deque
+from collections import deque
 #sys.stdin=open("input.txt", "r")
-
-
-dx=[-1,0,1,0]
+dx=[1,0,-1,0]
 dy=[0,-1,0,1]
-n = int(input())
+
+n=int(input())
 a = [list(map(int, input())) for _ in range(n)]
+res = []
 Q=deque()
-res=[]
 for i in range(n):
     for j in range(n):
-        if a[i][j] == 1:
-            cnt=1
+        if a[i][j]==1:
             Q.append((i,j))
+            cnt=1
             a[i][j]=0
             while Q:
                 tmp=Q.popleft()
@@ -21,41 +20,38 @@ for i in range(n):
                     x=tmp[0]+dx[k]
                     y=tmp[1]+dy[k]
                     if 0<=x<n and 0<=y<n and a[x][y]==1:
+                        cnt+=1
                         a[x][y]=0
                         Q.append((x,y))
-                        cnt+=1
             res.append(cnt)
 print(len(res))
 res.sort()
 for x in res:
     print(x)
 
+
 '''
-dx=[-1,0,1,0]
-dy=[0,-1,0,1]
 def DFS(x,y):
     global cnt
-    cnt += 1
     a[x][y]=0
     for k in range(4):
         xx=x+dx[k]
         yy=y+dy[k]
         if 0<=xx<n and 0<=yy<n and a[xx][yy]==1:
             DFS(xx,yy)
-
-
+            cnt+=1
 if __name__ == '__main__':
     n=int(input())
-    a=[list(map(int,input()))for _ in range(n)]
+    a=[list(map(int,input())) for _ in range(n)]
     res=[]
     for i in range(n):
         for j in range(n):
             if a[i][j]==1:
-                cnt=0
+                cnt=1
                 DFS(i,j)
                 res.append(cnt)
-    res.sort()
     print(len(res))
+    res.sort()
     for x in res:
         print(x)
 '''
--- python/14.안전영역.py
+++ python/14.안전영역.py
@@ -1,74 +1,35 @@
 import sys
 from collections import deque
-import copy
-#sys.stdin=open("input.txt", "r")
 sys.setrecursionlimit(10**6)
+#sys.stdin=open("input.txt", "r")
 
-dx=[-1, 0, 1, 0]
-dy=[0, 1, 0, -1]
+dx=[1,0,-1,0]
+dy=[0,-1,0,1]
+
 def DFS(x,y,h):
-    ch[x][y]=1
+    ch[x][y] = 1
     for k in range(4):
         xx=x+dx[k]
         yy=y+dy[k]
         if 0<=xx<n and 0<=yy<n and a[xx][yy]>h and ch[xx][yy]==0:
+            ch[xx][yy]=1
             DFS(xx,yy,h)
 
 
 if __name__ == '__main__':
     n=int(input())
     a=[list(map(int,input().split())) for _ in range(n)]
-    res=0
+    max=-2147000000
     for h in range(100):
         ch=[[0]*n for _ in range(n)]
         cnt=0
         for i in range(n):
             for j in range(n):
                 if a[i][j]>h and ch[i][j]==0:
-                    cnt+=1
                     DFS(i,j,h)
-        res=max(res,cnt)
+                    cnt+=1
+        if max<cnt:
+            max=cnt
         if cnt==0:
             break
-    print(res)
-
-
-'''
-dx=[0,1,0,-1]
-dy=[-1,0,1,0]
-n=int(input())
-a=[list(map(int,input().split())) for _ in range(n)]
-ch=copy.deepcopy(a)
-max=-2147000000
-min=2147000000
-for i in a:
-    for j in range(n):
-        if i[j]>max:
-            max=i[j]
-        if i[j]<min:
-            min=i[j]
-cnt=0
-Q=deque()
-m=0
-while min<=max:
-    cnt=0
-    for i in range(n):
-        for j in range(n):
-            if a[i][j]>min:
-                Q.append((i,j))
-                a[i][j]=0
-                while Q:
-                    tmp=Q.popleft()
-                    for k in range(4):
-                        x=tmp[0]+dx[k]
-                        y=tmp[1]+dy[k]
-                        if 0<=x<n and 0<=y<n and a[x][y]>min:
-                            Q.append((x,y))
-                            a[x][y]=0
-                cnt+=1
-    if cnt>m:
-        m=cnt
-    a=copy.deepcopy(ch)
-    min+=1
-print(m)
-'''
+print(max)
