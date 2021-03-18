a5067dc728d5de20a40680af74b5b351c97a0638
Jihyeon <gihyeon0715@naver.com>
2021-03-18 14:05:25 +0900

    secion7

--- python/8.사과나무.py
+++ python/8.사과나무.py
@@ -2,32 +2,29 @@ import sys
 from collections import deque
 #sys.stdin=open("input.txt", "r")
 
-dx=[-1,0,1,0]
-dy=[0,-1,0,1]
-
+dx=[0,1,0,-1]
+dy=[1,0,-1,0]
 n=int(input())
 a=[list(map(int,input().split())) for _ in range(n)]
-L=0
 ch=[[0]*n for _ in range(n)]
-sum=0
+L=0
 Q=deque()
-Q.append((n//2,n//2))
-sum+=a[n//2][n//2]
+sum=a[n//2][n//2]
 ch[n//2][n//2]=1
+Q.append((n//2,n//2))
 while True:
-    if L == n // 2:
+    if L==n//2:
         break
     else:
         size=len(Q)
         for _ in range(size):
             tmp=Q.popleft()
-            for i in range(4):
-                x=tmp[0]+dx[i]
-                y=tmp[1]+dy[i]
+            for k in range(4):
+                x=tmp[0]+dx[k]
+                y=tmp[1]+dy[k]
                 if 0<=x<n and 0<=y<n and ch[x][y]==0:
                     ch[x][y]=1
-                    sum+=a[x][y]
                     Q.append((x,y))
-            #print(sum)
+                    sum+=a[x][y]
         L+=1
 print(sum)
