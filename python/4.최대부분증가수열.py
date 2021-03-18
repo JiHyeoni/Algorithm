8eecc268fa3b0bf7a2a9f130fffc8ae762c2c40d
Jihyeon <gihyeon0715@naver.com>
2021-03-18 17:12:06 +0900

    secion8

--- python/4.최대부분증가수열.py
+++ python/4.최대부분증가수열.py
@@ -2,18 +2,18 @@ import sys
 #sys.stdin=open("input.txt", "r")
 
 n=int(input())
-arr=list(map(int,input().split()))
-arr.insert(0,0)
-res=0
+a=list(map(int,input().split()))
+a.insert(0,0)
+max=-2147000000
 dy=[0]*(n+1)
 dy[1]=1
-
 for i in range(2,n+1):
-    max=0
+    res=0
     for j in range(i-1,0,-1):
-        if arr[j]<arr[i] and dy[j]>max:
-            max=dy[j]
-    dy[i]=max+1
-    if dy[i]>res:
-        res=dy[i]
-print(res)
+        if a[j]<a[i] and dy[j]>res:
+            res=dy[j]
+    dy[i]=res+1
+    if dy[i]>max:
+        max=dy[i]
+print(max)
+
--- python/5.최대선연결하기.py
+++ python/5.최대선연결하기.py
@@ -2,17 +2,17 @@ import sys
 #sys.stdin=open("input.txt", "r")
 
 n=int(input())
-arr=list(map(int,input().split()))
-arr.insert(0,0)
-dy=[0]*(n+1)
-dy[1]=1
+a=list(map(int,input().split()))
+
+dy=[0]*(n)
+dy[0]=1
 res=0
-for i in range(2,n+1):
+for i in range(1,n):
     max=0
-    for j in range(i-1,0,-1):
-        if arr[j]<arr[i] and dy[j]>max:
+    for j in range(i-1,-1,-1):
+        if a[j]<a[i] and dy[j]>max:
             max=dy[j]
     dy[i]=max+1
-    if dy[i]>res:
+    if res<dy[i]:
         res=dy[i]
 print(res)
