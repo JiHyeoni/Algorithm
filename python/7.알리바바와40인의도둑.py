5505db0faa3b6e3dcb3cbed11a107d8759e691c3
Jihyeon <gihyeon0715@naver.com>
2021-03-23 14:40:43 +0900

    secion8

--- python/7.알리바바와40인의도둑.py
+++ python/7.알리바바와40인의도둑.py
@@ -18,27 +18,23 @@ for i in range(1,n):
             dy[i][j] = dy[i-1][j] + a[i][j]
 print(dy[n-1][n-1])
 '''
-import sys
-#sys.stdin=open("input.txt", "r")
-
 def DFS(x,y):
     if dy[x][y]>0:
         return dy[x][y]
     if x==0 and y==0:
-        return arr[0][0]
+        return a[0][0]
     else:
-        if y==0:
-            dy[x][y]=DFS(x-1,y)+arr[x][y]
-        elif x==0:
-            dy[x][y]=DFS(x,y-1)+arr[x][y]
+        if x==0:
+            dy[x][y]=DFS(x,y-1)+a[x][y]
+        elif y==0:
+            dy[x][y]=DFS(x-1,y)+a[x][y]
         else:
-            dy[x][y]=min(DFS(x-1,y),DFS(x,y-1))+arr[x][y]
+            dy[x][y]=min(DFS(x-1,y),DFS(x,y-1))+a[x][y]
         return dy[x][y]
 
 if __name__ == '__main__':
     n=int(input())
-    arr=[list(map(int,input().split())) for _ in range(n)]
+    a=[list(map(int,input().split())) for _ in range(n)]
     dy=[[0]*n for _ in range(n)]
     print(DFS(n-1,n-1))
-
 '''
--- python/9.가방문제.py
+++ python/9.가방문제.py
@@ -1,11 +1,12 @@
 import sys
-#sys.stdin=open("input.txt", "r")
+#sys.stdin = open("input.txt", 'r')
 
 if __name__ == '__main__':
     n,m=map(int,input().split())
     dy=[0]*(m+1)
-    for i in range(n):
+    for _ in range(n):
         w,v=map(int,input().split())
         for j in range(w,m+1):
             dy[j]=max(dy[j],dy[j-w]+v)
+
     print(dy[m])
