48a10ab751cdc66c78b57e2a81f41356acbeaf15
Jihyeon <gihyeon0715@naver.com>
2021-03-24 21:46:49 +0900

    secion8

--- python/4.최대부분증가수열.py
+++ python/4.최대부분증가수열.py
@@ -1,18 +1,15 @@
 import sys
-#sys.stdin=open("input.txt", "r")
+#sys.stdin = open("input.txt", 'r')
 
 n=int(input())
 a=list(map(int,input().split()))
 dy=[0]*n
-dy[0]=1
-max=0
-
-for i in range(1,n):
-    res=0
+res=0
+for i in range(n):
+    cnt=0
     for j in range(i-1,-1,-1):
-        if a[i]>a[j] and res<dy[j]:
-            res=dy[j]
-    dy[i]=res+1
-    if max<dy[i]:
-        max=dy[i]
-print(max)
+        if a[i]>a[j] and dy[j]>cnt:
+            cnt=dy[j]
+        dy[i]=cnt+1
+    res=max(dy[i],res)
+print(res)
