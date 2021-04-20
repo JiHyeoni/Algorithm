91dd31e49b99ab850f16ac1bc6a11e810ab9eae6
Jihyeon <gihyeon0715@naver.com>
2021-04-20 15:19:35 +0900

    section1

---
+++ java/7.회문문자열.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+
+	public String solution(String str) {
+		String answer="NO";
+		
+		String tmp=new StringBuilder(str).reverse().toString();
+		if(str.equalsIgnoreCase(tmp))
+		{
+			answer="YES";
+		}
+		
+		/*
+		 * String answer="YES";
+		str=str.toUpperCase();
+		int len=str.length();
+		for (int i =0; i<len/2;i++)
+		{
+			if (str.charAt(i)!=str.charAt(len-i-1)) answer="NO";
+		}*/
+		
+		/*
+		str=str.toUpperCase();
+		char s[]=str.toCharArray();
+		int lt=0, rt=str.length()-1;
+		while(lt<rt)
+		{
+			if (s[lt]==s[rt])
+			{
+				lt++;
+				rt--;
+			}
+			else
+			{
+				answer="NO";
+				return answer;
+			}
+		}
+		answer="YES";
+		*/
+		return answer;
+	}
+	public static void main(String[] args) {
+		
+		Main T=new Main();
+		Scanner kb=new Scanner(System.in);
+		String str=kb.next();
+		System.out.print(T.solution( str));
+				
+	}
+	
+}
