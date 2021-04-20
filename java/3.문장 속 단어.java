1d73d38094b41584f32c024f31cf972a9d5ed666
Jihyeon <gihyeon0715@naver.com>
2021-04-20 14:18:23 +0900

    section1

--- java/3.문장 속 단어.java
+++ java/3.문장 속 단어.java
@@ -4,10 +4,13 @@ public class Main {
 
 	public String solution(String str) {
 		String answer="";
-		int m=Integer.MIN_VALUE, pos;
-		while((pos=str.indexOf(' '))!=-1) {
+		int m = Integer.MIN_VALUE, pos;
+		
+		while((pos=str.indexOf(' '))!=-1)
+		{
 			String tmp=str.substring(0,pos);
 			int len=tmp.length();
+			
 			if(len>m)
 			{
 				m=len;
@@ -15,16 +18,23 @@ public class Main {
 			}
 			str=str.substring(pos+1);
 		}
-		if (str.length()>m) answer=str;
-		/*String[] s=str.split(" ");
-		for(String x:s) {
+		if(str.length()>m) answer=str;
+		
+		/*
+		int m=Integer.MIN_VALUE;
+		String s[]=str.split(" ");
+		
+		for (String x : s )
+		{
 			int len=x.length();
-			if(len>m) {
+			if (m<len)
+			{
 				m=len;
 				answer=x;
 			}
-		}*/
-
+		}
+		*/
+		
 		return answer;
 	}
 	public static void main(String[] args) {
@@ -32,9 +42,10 @@ public class Main {
 		Main T=new Main();
 		Scanner kb=new Scanner(System.in);
 		String str=kb.nextLine();
+		
 		System.out.print(T.solution(str));
 		System.out.println();
-		
+				
 	}
 	
 }
