49acc1735bed54c9bd29625f5318942c82c04799
Jihyeon <gihyeon0715@naver.com>
2021-04-20 13:39:09 +0900

    section1

--- java/2.대소문자 변환.java
+++ java/2.대소문자 변환.java
@@ -4,10 +4,22 @@ public class Main {
 
 	public String solution(String str) {
 		String answer="";
-		for (char x : str.toCharArray()){
-			if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x);
-			else answer+=Character.toLowerCase(x);
+		char ch[]=str.toCharArray();
+		for (char x : ch) {
+			if (Character.isUpperCase(x)) {
+				answer=answer+Character.toLowerCase(x);
+			}
+			else
+			{
+				answer=answer+Character.toUpperCase(x);
+			}
 		}
+		/*
+		for(char x : str.toCharArray()){
+			if(x>=97 && x<=122) answer+=(char)(x-32);
+			else answer+=(char)(x+32);
+		}
+		 */
 
 		return answer;
 	}
@@ -16,10 +28,10 @@ public class Main {
 		Main T=new Main();
 		Scanner kb=new Scanner(System.in);
 		String str=kb.next();
+		
 		System.out.print(T.solution(str));
 		System.out.println();
-
-		
+				
 	}
 	
 }
