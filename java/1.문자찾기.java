e13ab675aa3e771743f65f7dba8b45d3d3f74f3d
Jihyeon <gihyeon0715@naver.com>
2021-04-20 13:04:58 +0900

    section1

--- java/1.문자찾기.java
+++ java/1.문자찾기.java
@@ -2,17 +2,20 @@ import java.util.*;
 
 public class Main {
 
-	public int solution(String str, char t) {
+	public int solution(String str,char t) {
 		int answer=0;
 		str=str.toUpperCase();
-		t=Character.toUpperCase(t);
-		//System.out.println(str+" "+t);
-		/*for (int i=0; i<str.length();i++) {
-			if(str.charAt(i)==t) answer++;
-		}*/
-		for(char x : str.toCharArray()) {
-			if (x==t) answer++;
+		t = Character.toUpperCase(t);
+		
+		for ( int i =0 ;i<str.length();i++)
+		{
+			if (str.charAt(i)==t) answer++;
 		}
+		
+		/*for (char x : str.toCharArray()) {
+			if (x==t) answer++;
+		}*/
+		
 		return answer;
 	}
 	public static void main(String[] args) {
@@ -20,10 +23,10 @@ public class Main {
 		Main T=new Main();
 		Scanner kb=new Scanner(System.in);
 		String str=kb.next();
-		char c=kb.next().charAt(0);
-		System.out.print(T.solution(str,c));
+		char ch=kb.next().charAt(0);
+		System.out.print(T.solution(str,ch));
 		System.out.println();
-		
+				
 	}
 	
 }
