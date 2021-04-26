664b3c9ef3343fc2b383550e84caba4717866163
Jihyeon <gihyeon0715@naver.com>
2021-04-26 17:48:57 +0900

    section2

--- java/5.소수(에라토스테네스 체).java
+++ java/5.소수(에라토스테네스 체).java
@@ -2,30 +2,30 @@ import java.util.*;
 
 public class Main {
 
-	public int  solution(int n) {
+
+	public int solution( int n) {
 		int answer=0;
-		int [] arr=new int[n+1];
-		
-		for(int i=2;i<=n;i++)
-		{
-			if(arr[i]==0)
-			{
-				for(int j=i;j<=n;j=j+i)
-				{
-				arr[j]=1;
+		int []arr=new int[n+1];
+		for (int i=2;i<n+1;i++) {
+			if(arr[i]==0) {
+				for(int j=i;j<n+1;j=j+i) {
+					arr[j]=1;
 				}
 				answer++;
 			}
 		}
 
+		
 		return answer;
 	}
+	
 	public static void main(String[] args) {
 		
 		Main T=new Main();
 		Scanner kb=new Scanner(System.in);
 		int n=kb.nextInt();
 		System.out.print(T.solution(n));
-	}
-	
 }
+}
+	
+
