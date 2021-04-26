6f24281e711d82d82cb385bc6f87985a22bee88d
Jihyeon <gihyeon0715@naver.com>
2021-04-26 18:03:58 +0900

    section2

--- java/6.뒤집은 소수.java
+++ java/6.뒤집은 소수.java
@@ -1,44 +1,49 @@
 import java.util.*;
 
 public class Main {
-	public boolean isPrime(int num) {
-		if(num==1) return false;
-		for(int i=2;i<num;i++) {
-			if(num%i==0) return false;
+	
+	public boolean isPrime(int a) {
+		
+		if (a==1) return false;
+		
+		for(int i=2;i<a;i++) {
+			if(a%i==0) return false;
 		}
+		
 		return true;
+		
 	}
 
-	public ArrayList<Integer>  solution(int n,int[] m) {
+
+	public ArrayList<Integer> solution( int n,int[] arr) {
 		ArrayList<Integer> answer=new ArrayList<>();
+		
 		for(int i=0;i<n;i++) {
-			int tmp=m[i];
-			int res=0;
+			int tmp=arr[i];
+			int a=0;
 			while(tmp>0) {
-				int t=tmp%10;
-				res=res*10+t;
+				a=10*a+tmp%10;
 				tmp=tmp/10;
 			}
-			if(isPrime(res)) answer.add(res);
+			if(isPrime(a)) answer.add(a);
 		}
-
+				
 		return answer;
 	}
+	
 	public static void main(String[] args) {
 		
 		Main T=new Main();
 		Scanner kb=new Scanner(System.in);
 		int n=kb.nextInt();
-		int []m=new int[n];
-		for(int i=0;i<n;i++)
-		{
-			m[i]=kb.nextInt();
+		int []arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=kb.nextInt();
 		}
-		
-		for(int x: T.solution(n,m))
-		{
+		for(int x:T.solution(n,arr)) {
 			System.out.print(x+" ");
 		}
-	}
-	
 }
+}
+	
+
