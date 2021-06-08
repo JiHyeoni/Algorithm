264a263297a39391efe9e81e27d9279994ccad9b
Jihyeon <gihyeon0715@naver.com>
2021-06-08 17:29:11 +0900

    section1

--- java/12.암호.java
+++ java/12.암호.java
@@ -1,28 +1,44 @@
 import java.util.*;
 
 public class Main {
-
-	public String solution(int n, String str) {
+	public String  solution(int n, String str) {
 		String answer="";
-		for(int i=0;i<n;i++)
-			{
-			String tmp=str.substring(0,7).replace('#', '1').replace('*', '0');
-			int num=Integer.parseInt(tmp,2);
-			answer+=(char)num;
-			str=str.substring(7);
-			
+		/*
+		 * int m=str.length()/n;
+		int a=0;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				if(str.charAt(j)=='#') {
+					a=a*2+1;
+				}
+				else if(str.charAt(j)=='*') {
+					a=a*2;
+				}
 			}
-
-
+			answer+=(char)a;
+			a=0;
+			str=str.substring(m);
+		}	
+		*/
+		for(int i=0;i<n;i++) {
+		String tmp=str.substring(0,7).replace('#','1').replace('*', '0');
+		int num=Integer.parseInt(tmp,2);
+		answer+=(char)num;
+		str=str.substring(7);
+		}
+		
 		return answer;
 	}
+	
 	public static void main(String[] args) {
 		
-		Main T=new Main();
-		Scanner kb=new Scanner(System.in);
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
 		int n=kb.nextInt();
 		String str=kb.next();
-		System.out.print(T.solution(n,str));
-	}
-	
+		
+		System.out.print(T.solution(n, str));
 }
+}
+	
+
