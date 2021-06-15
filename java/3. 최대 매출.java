14f38f0835bc28f99fae7458f1e418afeb3dbe03
Jihyeon <gihyeon0715@naver.com>
2021-06-15 13:31:34 +0900

    section3

---
+++ java/3. 최대 매출.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+
+	public int  solution(int n,int m, int [] arr) {
+			int answer=0, sum=0;
+			for(int i=0;i<m;i++) sum+=arr[i];
+			answer=sum;
+			for(int i=m;i<n;i++) 
+			{
+				sum+=(arr[i]-arr[i-m]);
+				answer=Math.max(answer, sum);
+			}
+			
+			/*
+			 * for(int i=0;i<n-(m+1);i++) {
+				int cnt=0;
+				for(int j=0;j<m;j++) {
+					cnt+=arr[i+j];
+				}
+				if(cnt>answer) {
+					answer=cnt;
+				}
+			}
+			*/
+			
+			return answer;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int m=kb.nextInt();
+		int[]arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=kb.nextInt();
+		}
+		System.out.print(T.solution(n, m, arr));
+		
+}
+}
+	
+
