436e2e74a26275d32116cf8181d9c2f24917f271
Jihyeon <gihyeon0715@naver.com>
2021-06-24 11:42:22 +0900

    section5

---
+++ java/1. 올바른 괄호.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main {	
+	public String solution(String s){
+		String answer="YES";
+		Stack<Character> stack=new Stack<>();
+		
+		for(char x : s.toCharArray()) {
+			if(x=='(') stack.push(x);
+			else if(x==')') {
+				if(stack.empty()) return "NO";
+				else stack.pop();
+			}
+		}
+		
+		if(!stack.empty()) return "NO";
+		
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String s=kb.next();
+	
+		System.out.println(T.solution(s));
+	}
+}
