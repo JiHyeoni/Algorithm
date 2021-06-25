14e2a95805d40586339b86657345b1ed79e5dd19
Jihyeon <gihyeon0715@naver.com>
2021-06-25 10:38:27 +0900

    section5

--- java/4. 후위식 연산.java
+++ java/4. 후위식 연산.java
@@ -2,21 +2,21 @@ import java.util.*;
 class Main {	
 	public int solution(String str){
 		int answer=0;
-		Stack<Integer> stack=new Stack<>();
+		Stack <Integer> stack=new Stack<>();
 		for(char x : str.toCharArray()) {
-			if(Character.isDigit(x)) stack.push(Character.getNumericValue(x));
+			
+			if(Character.isDigit(x)) stack.push(x-48);
 			else {
 				int num1=stack.pop();
 				int num2=stack.pop();
-				
-				if(x=='*') answer=num2*num1;
-				else if (x=='/') answer=num2/num1;
-				else if (x=='-') answer=num2-num1;
-				else if(x=='+') answer=num2+num1;
-				stack.push(answer);
-			}
+				if(x=='+') stack.push(num2+num1);
+				else if(x=='-') stack.push(num2-num1);
+				else if(x=='*') stack.push(num2*num1);
+				else if(x=='/') stack.push(num2/num1);
+					}
 		}
 		answer=stack.pop();
+
 		return answer;
 	}
 
@@ -24,6 +24,7 @@ class Main {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
 		String str=kb.next();
+		
 		System.out.println(T.solution(str));
 	}
 }
