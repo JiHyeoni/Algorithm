0794c1ec7b9c0f8c2518a59bc9da836a0fe52138
Jihyeon <gihyeon0715@naver.com>
2021-06-25 16:46:10 +0900

    section5

---
+++ java/7. 교육과정설계.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Main {	
+	public String solution(String str1, String str2){
+		String answer="YES";
+		Queue <Character> Q=new LinkedList<>();
+		for (char x : str1.toCharArray()) {
+			Q.offer(x);
+		}
+		for(char x: str2.toCharArray()) {
+			if(Q.contains(x)) {
+				if(x!=Q.poll()) return "NO";
+			}
+		}
+		if(!Q.isEmpty()) return "NO";
+		
+		/*
+		Queue <Character> Q1=new LinkedList<>();
+		Queue <Character> Q2=new LinkedList<>();
+		
+		for(char x : str1.toCharArray()) {
+			Q1.offer(x);
+		}
+		for (char x : str2.toCharArray()) {
+			Q2.offer(x);
+		}
+		
+		while(!Q2.isEmpty()) {
+			if(Q2.poll()==Q1.peek()) Q1.poll();
+		}
+		
+		if(Q1.isEmpty()) answer="YES";
+		else answer="NO";
+		*/
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+
+		String str1=kb.next();
+		String str2=kb.next();
+		
+		System.out.println(T.solution(str1,str2));
+	}
+}
