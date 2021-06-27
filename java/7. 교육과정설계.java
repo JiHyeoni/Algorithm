58a67b76f497735c75204c2ee5c0e493ef7c27ec
Jihyeon <gihyeon0715@naver.com>
2021-06-27 12:34:19 +0900

    section5

--- java/7. 교육과정설계.java
+++ java/7. 교육과정설계.java
@@ -44,3 +44,35 @@ class Main {
 		System.out.println(T.solution(str1,str2));
 	}
 }
+
+/*
+import java.util.*;
+class Main {	
+
+	public String solution(String str1,String str2){
+		String answer="YES";
+		Queue <Character> Q=new LinkedList<>();
+		for(char x : str1.toCharArray()) Q.offer(x);
+		
+		for(char x:str2.toCharArray()) {
+			if(Q.contains(x)) {
+				if(Q.peek()==x) Q.poll();
+				else return "NO";
+			}
+		}
+		if(!Q.isEmpty()) return "NO";
+		
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+	
+		String str1=kb.next();
+		String str2=kb.next();
+		
+		System.out.println(T.solution(str1,str2));
+	}
+}
+*/
