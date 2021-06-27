a2ad68f4d2ce68ed2ed251abdfb2f35aafbbeea0
Jihyeon <gihyeon0715@naver.com>
2021-06-27 12:56:44 +0900

    section5

--- java/8. 응급실.java
+++ java/8. 응급실.java
@@ -1,19 +1,21 @@
 import java.util.*;
-class Main {	
 
-	class Person{
-		int id;
-		int priority;
-		public Person(int id,int priority) {
-			this.id=id;
-			this.priority=priority;
-		}
+class Person{
+	int id;
+	int priority;
+	
+	public Person(int id,int priority) {
+		this.id=id;
+		this.priority=priority;
 	}
-	public int solution(int n,int m, int[] arr){
+}
+
+class Main {	
+	public int solution(int n,int m, int []arr){
 		int answer=0;
 		Queue <Person> Q=new LinkedList<>();
 		for(int i=0;i<n;i++) Q.offer(new Person(i,arr[i]));
-		
+
 		while(!Q.isEmpty()) {
 			Person tmp=Q.poll();
 			for(Person x:Q) {
@@ -21,13 +23,14 @@ class Main {
 					Q.offer(tmp);
 					tmp=null;
 					break;
+					}
 				}
-			}
 			if(tmp!=null) {
 				answer++;
 				if(tmp.id==m) return answer;
 			}
-		}
+				
+			}
 		
 		return answer;
 	}
@@ -38,9 +41,10 @@ class Main {
 
 		int n=kb.nextInt();
 		int m=kb.nextInt();
-		
-		int [] arr=new int[n];
-		for(int i=0;i<n;i++) arr[i]=kb.nextInt();
+		int [] arr= new int [n];
+		for(int i=0;i<n;i++) {
+			arr[i]=kb.nextInt();
+		}
 		
 		System.out.println(T.solution(n,m,arr));
 	}
