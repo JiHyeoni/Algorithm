adf10da90d3fd96b30b95b0ff856c471171c68da
Jihyeon <gihyeon0715@naver.com>
2021-07-01 10:21:24 +0900

    section6

--- java/4. LRU.java
+++ java/4. LRU.java
@@ -1,31 +1,41 @@
 import java.util.*;
+
 class Main {	
-	public int[] solution(int size, int n, int[] arr){
-		int[] cache=new int[size];
-		for(int x : arr){
+	public int[] solution( int s, int n, int []arr){
+		int [] answer=new int[s];
+		for(int x:arr) {
 			int pos=-1;
-			for(int i=0; i<size; i++) if(x==cache[i]) pos=i;
-			if(pos==-1){
-				for(int i=size-1; i>=1; i--){
-					cache[i]=cache[i-1];
+			for(int i=0;i<s;i++) {
+				if(answer[i]==x) pos=i;
+					}
+			if(pos==-1) {
+				for(int i=s-1;i>=1;i--) {
+					answer[i]=answer[i-1];
 				}
 			}
-			else{
-				for(int i=pos; i>=1; i--){
-					cache[i]=cache[i-1];
+			else {
+				for(int i=pos;i>=1;i--) {
+					answer[i]=answer[i-1];
 				}
 			}
-			cache[0]=x;
+			answer[0]=x;
 		}
-		return cache;
+		return answer;
 	}
+
 	public static void main(String[] args){
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
+		
 		int s=kb.nextInt();
 		int n=kb.nextInt();
-		int[] arr=new int[n];
-		for(int i=0; i<n; i++) arr[i]=kb.nextInt();
-		for(int x : T.solution(s, n, arr)) System.out.print(x+" ");
+		int [] arr= new int [n];
+		for(int i=0;i<n;i++) {
+			arr[i]=kb.nextInt();
+		}
+		
+		for(int x: T.solution(s, n, arr)) {
+			System.out.print(x+" ");
+		}
 	}
 }
