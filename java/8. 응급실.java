00d45a978fcb6ad2c45dbcf4c51e7b7c572e58c0
Jihyeon <gihyeon0715@naver.com>
2021-07-01 11:00:18 +0900

    section5

--- java/8. 응급실.java
+++ java/8. 응급실.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Person{
 	int id;
 	int priority;
-	
 	public Person(int id,int priority) {
 		this.id=id;
 		this.priority=priority;
@@ -11,11 +10,11 @@ class Person{
 }
 
 class Main {	
-	public int solution(int n,int m, int []arr){
+	public int solution( int n, int m,int []arr){
 		int answer=0;
-		Queue <Person> Q=new LinkedList<>();
+		Queue<Person> Q=new LinkedList<>();
 		for(int i=0;i<n;i++) Q.offer(new Person(i,arr[i]));
-
+		
 		while(!Q.isEmpty()) {
 			Person tmp=Q.poll();
 			for(Person x:Q) {
@@ -23,22 +22,21 @@ class Main {
 					Q.offer(tmp);
 					tmp=null;
 					break;
-					}
 				}
+			}
 			if(tmp!=null) {
 				answer++;
-				if(tmp.id==m) return answer;
+				if(m==tmp.id) return answer;
 			}
-				
-			}
-		
+		}
+
 		return answer;
 	}
 
 	public static void main(String[] args){
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-
+		
 		int n=kb.nextInt();
 		int m=kb.nextInt();
 		int [] arr= new int [n];
@@ -46,6 +44,6 @@ class Main {
 			arr[i]=kb.nextInt();
 		}
 		
-		System.out.println(T.solution(n,m,arr));
+		System.out.print(T.solution(n,m, arr));
 	}
 }
