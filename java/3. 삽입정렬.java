32aa3266ea9cef115f4f1934110d9e64a37121d5
Jihyeon <gihyeon0715@naver.com>
2021-07-15 13:29:19 +0900

    section6

--- java/3. 삽입정렬.java
+++ java/3. 삽입정렬.java
@@ -1,20 +1,10 @@
 import java.util.*;
 
+
+
 class Main {	
-	public int[] solution(int n, int []arr){
-		/*for(int i=1;i<n;i++) {
-			int idx=i;
-			for(int j=i-1;j>=0;j--) {
-				if(arr[idx]<arr[j]) {			
-					int tmp=arr[idx];
-					arr[idx]=arr[j];
-					arr[j]=tmp;
-					idx=j;
-				}
-				else break;
-			}
-		}*/
-		
+	
+	public int[] solution (int n,int []arr) {
 		for(int i=1;i<n;i++) {
 			int tmp=arr[i],j;
 			for(j=i-1;j>=0;j--) {
@@ -24,59 +14,22 @@ class Main {
 			arr[j+1]=tmp;
 		}
 		
-		
 		return arr;
-	}
-
-	public static void main(String[] args){
-		Main T = new Main();
-		Scanner kb = new Scanner(System.in);
-
-		int n=kb.nextInt();
-		int [] arr= new int [n];
-		for(int i=0;i<n;i++) {
-			arr[i]=kb.nextInt();
-		}
-		
-		for(int x: T.solution(n,arr)) {
-			System.out.print(x+" ");
-		}
-	}
-}
-
-
-
-/*
-import java.util.*;
-
-class Main {	
-	public int[] solution( int n, int []arr){
-		
-		for(int i=1;i<n;i++) {
-			int tmp=arr[i],j;
-			for(j=i-1;j>=0;j--) {
-				if(tmp<arr[j]) arr[j+1]=arr[j];
-				else break;
-			}
-			arr[j+1]=tmp;
-		}
-		return arr;
-	}
-
+	 }
+	
 	public static void main(String[] args){
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
 		
-
 		int n=kb.nextInt();
-		int [] arr= new int [n];
+		int [] arr=new int[n];
+		
 		for(int i=0;i<n;i++) {
 			arr[i]=kb.nextInt();
 		}
 		
-		for(int x: T.solution(n, arr)) {
+		for(int x : T.solution(n, arr)) {
 			System.out.print(x+" ");
 		}
 	}
 }
-*/
