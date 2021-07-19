56149a15acb6db429aefe4e6a74eec902ea9a766
Jihyeon <gihyeon0715@naver.com>
2021-07-19 14:09:11 +0900

    section6

---
+++ java/8. 이분검색.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+
+
+class Main {	
+	public int solution(int n,int m,int [] arr)
+	{
+		int answer=0;
+		int lt=0;
+		int rt=n-1;
+		Arrays.sort(arr);
+		while(lt<=rt) {
+			int mid=(lt+rt)/2;
+			if(arr[mid]==m) {
+				answer=mid+1;
+				break;
+			}
+			else if(arr[mid]>m) {
+				rt=mid;
+	
+			}
+			else if(arr[mid]<m) {
+				lt=mid;
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+
+		int n=kb.nextInt();
+		int m= kb.nextInt();
+		int [] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=kb.nextInt();
+		}
+		System.out.print(T.solution(n, m, arr));
+	}
+}
