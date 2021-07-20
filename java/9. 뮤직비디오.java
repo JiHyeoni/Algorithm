7f1bd230c3addde6eaaf63d786abbaa45f5fc287
Jihyeon <gihyeon0715@naver.com>
2021-07-20 15:12:30 +0900

    section6

--- java/9. 뮤직비디오.java
+++ java/9. 뮤직비디오.java
@@ -1,30 +1,30 @@
 import java.util.*;
 
 
-
 class Main {	
 	
-	public int count(int[]arr,int capacity) {
-		int cnt=1, sum=0;
+	public int count(int capacity,int [] arr) {
+		int cnt=1;
+		int sum=0;
 		for(int x:arr) {
-			if(sum+x>capacity) {
+			if(x+sum>capacity) {
 				cnt++;
 				sum=x;
 			}
 			else sum+=x;
 		}
 		
-		return cnt;	
+		return cnt;
 	}
 	
-	public int solution(int n,int m,int [] arr)
-	{
+	public int solution(int n, int m,int [] arr) {
 		int answer=0;
 		int lt=Arrays.stream(arr).max().getAsInt();
 		int rt=Arrays.stream(arr).sum();
+		
 		while(lt<=rt) {
-			int mid=(rt+lt)/2;
-			if(count(arr,mid)<=m) {
+			int mid=(lt+rt)/2;
+			if(count(mid,arr)<=m) {
 				answer=mid;
 				rt=mid-1;
 			}
@@ -32,20 +32,22 @@ class Main {
 				lt=mid+1;
 			}
 		}
-
+		
 		return answer;
 	}
-	
+
 	public static void main(String[] args){
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
 
 		int n=kb.nextInt();
-		int m= kb.nextInt();
+		int m=kb.nextInt();
 		int [] arr=new int[n];
+		
 		for(int i=0;i<n;i++) {
 			arr[i]=kb.nextInt();
 		}
-		System.out.print(T.solution(n, m, arr));
+		
+		System.out.println(T.solution(n, m, arr));
 	}
 }
