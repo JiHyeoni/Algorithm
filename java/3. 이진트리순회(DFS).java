a69a17e1f43a66630d83ae1c0abc2b402543b49a
Jihyeon <gihyeon0715@naver.com>
2021-07-26 18:15:26 +0900

    section7

--- java/3. 이진트리순회(DFS).java
+++ java/3. 이진트리순회(DFS).java
@@ -4,24 +4,29 @@ class Node{
 	int data;
 	Node lt,rt;
 	public Node(int val) {
-		data=val;
-		lt=rt=null;
+		this.data=val;
+		lt=null;
+		rt=null;
 	}
 }
 
 class Main {	
 	Node root;
 	public void DFS(Node root) {
+		
 		if(root==null) return;
 		else {
 			DFS(root.lt);
-			System.out.print(root.data+" ");
 			DFS(root.rt);
+			System.out.print(root.data+" ");
 		}
+
 	}
-		
+	
 	public static void main(String[] args){
 		Main T = new Main();
+		Scanner kb=new Scanner(System.in);
+		
 		T.root=new Node(1);
 		T.root.lt=new Node(2);
 		T.root.rt=new Node(3);
@@ -29,7 +34,7 @@ class Main {
 		T.root.lt.rt=new Node(5);
 		T.root.rt.lt=new Node(6);
 		T.root.rt.rt=new Node(7);
+		
 		T.DFS(T.root);
-
 	}
 }
