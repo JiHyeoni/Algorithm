97f20e156a201656a071c4fe6eb5d9b8d9ef748c
Jihyeon <gihyeon0715@naver.com>
2021-08-05 14:42:07 +0900

    section7

--- java/9. 부분집합 구하기(DFS).java
+++ java/9. 부분집합 구하기(DFS).java
@@ -3,16 +3,14 @@ import java.util.*;
 
 class Main {	
 	static int n;
-	static int[]ch;
+	static int[] ch;
 	public void DFS(int L) {
-		
 		if(L==n+1) {
 			String tmp=" ";
 			for(int i=1;i<n+1;i++) {
 				if(ch[i]==1) tmp+=(i+" ");
 			}
 			if(tmp.length()>0) System.out.println(tmp);
-			
 		}
 		else {
 			ch[L]=1;
@@ -20,6 +18,7 @@ class Main {
 			ch[L]=0;
 			DFS(L+1);
 		}
+
 	}
 	
 	public static void main(String[] args){
@@ -27,7 +26,7 @@ class Main {
 		Scanner kb=new Scanner(System.in);
 		n=kb.nextInt();
 		ch=new int[n+1];
-		
 		T.DFS(1);
+		
 	}
 }
