8cb9a5300a6832655e04728e74287d83d3a29ad2
Jihyeon <gihyeon0715@naver.com>
2021-08-17 12:16:11 +0900

    section7

--- java/8. 말단노드까지의 까장 짧은 경로(BFS).java
+++ java/8. 말단노드까지의 까장 짧은 경로(BFS).java
@@ -3,7 +3,7 @@ import java.util.*;
 class Node{
 	int data;
 	Node lt,rt;
-	public Node(int val){
+	public Node(int val) {
 		this.data=val;
 		lt=rt=null;
 	}
@@ -11,26 +11,25 @@ class Node{
 
 class Main {
 	Node root;
-	
-	public int BFS(Node root) {
-		Queue<Node> Q=new LinkedList<>();
-		int L=0;
+	public int BFS(Node root){
+		Queue <Node> Q=new LinkedList<>();
 		Q.offer(root);
+		int L=0;
 		
 		while(!Q.isEmpty()) {
 			int len=Q.size();
 			for(int i=0;i<len;i++) {
 				Node cur=Q.poll();
-				if(cur.lt==null && cur.rt==null) return L;
 				if(cur.lt!=null) Q.offer(cur.lt);
 				if(cur.rt!=null) Q.offer(cur.rt);
+				if(cur.lt==null && cur.rt==null) return L;
 			}
 			L++;
 		}
-		return 0;
-	}
 
-	
+		return 0;
+		}
+
 	public static void main(String[] args){
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
@@ -39,7 +38,7 @@ class Main {
 		T.root.rt=new Node(3);
 		T.root.lt.lt=new Node(4);
 		T.root.lt.rt=new Node(5);
-
-		System.out.print(T.BFS(T.root));
+		
+		System.out.println(T.BFS(T.root));
 	}
 }
