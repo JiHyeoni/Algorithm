0a2da0a4e0148e628b30a4098d0cd07e0057ebfd
Jihyeon <gihyeon0715@naver.com>
2021-08-18 15:15:31 +0900

    section7

--- java/10. 송아지 찾기 1(BFS).java
+++ java/10. 송아지 찾기 1(BFS).java
@@ -1,25 +1,21 @@
 import java.util.*;
 
-
-
 class Main {
-	int [] dir= {-1,1,5};
+	int [] dis= {-1,1,5};
 	int [] ch;
 	public int BFS(int s, int e){
-		Queue<Integer> Q=new LinkedList<>();
-		ch = new int[10001];
+		Queue <Integer> Q = new LinkedList<>();
+		ch=new int[10001];
 		int L=0;
 		Q.offer(s);
 		ch[s]=1;
 		while(!Q.isEmpty()) {
 			int len=Q.size();
 			for(int i=0;i<len;i++) {
-				int cur=Q.poll();
+				int x=Q.poll();
 				for(int j=0;j<3;j++) {
-					int nx=cur+dir[j];
-					if(nx==e) {
-						return L+1;
-					}
+					int nx=x+dis[j];
+					if(nx==e) return L+1;
 					if(nx>=1 && nx<=10000 && ch[nx]==0) {
 						ch[nx]=1;
 						Q.offer(nx);
@@ -31,14 +27,11 @@ class Main {
 		return 0;
 		}
 
-	
 	public static void main(String[] args){
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
 		int s=kb.nextInt();
 		int e=kb.nextInt();
-		
-		System.out.print(T.BFS(s,e));
-
+		System.out.println(T.BFS(s,e));
 	}
 }
