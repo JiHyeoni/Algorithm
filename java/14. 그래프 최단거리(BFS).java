88348b540a992bce602a1429c222fc5b9aad3d24
Jihyeon <gihyeon0715@naver.com>
2021-08-20 12:32:45 +0900

    section7

--- java/14. 그래프 최단거리(BFS).java
+++ java/14. 그래프 최단거리(BFS).java
@@ -5,11 +5,10 @@ import java.util.*;
 class Main {
 	static int n,m;
 	static ArrayList<ArrayList<Integer>> graph;
-	static int[] dis;
 	static int [] ch;
-	
-	public void BFS(int v) {
-		Queue <Integer> Q=new LinkedList<>();
+	static int [] dis;
+	public static void BFS(int v) {
+		Queue<Integer> Q = new LinkedList<>();
 		ch[v]=1;
 		dis[v]=0;
 		Q.offer(v);
@@ -18,25 +17,28 @@ class Main {
 			for(int nv:graph.get(cv)) {
 				if(ch[nv]==0) {
 					ch[nv]=1;
-					Q.offer(nv);
 					dis[nv]=dis[cv]+1;
+					Q.offer(nv);
 				}
 			}
 		}
-		}
-
+	}
 	
 	public static void main(String[] args){
+		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
 		n=kb.nextInt();
 		m=kb.nextInt();
-		ch=new int[n+1];
-		dis=new int[n+1];
 		graph=new ArrayList<ArrayList<Integer>>();
+		
 		for(int i=0;i<=n;i++) {
 			graph.add(new ArrayList<Integer>());
 		}
+		
+		ch=new int[n+1];
+		dis=new int[n+1];
+		
 		for(int i=0;i<m;i++) {
 			int a=kb.nextInt();
 			int b=kb.nextInt();
@@ -48,5 +50,6 @@ class Main {
 		for(int i=2;i<=n;i++) {
 			System.out.println(i+" : "+dis[i]);
 		}
-	}
+		
+}
 }
