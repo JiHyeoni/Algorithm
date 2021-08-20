5e67ceed2ad6f56d810a0073886aad2f9d084387
Jihyeon <gihyeon0715@naver.com>
2021-08-20 10:54:19 +0900

    section7

--- java/13. 경로탐색(인접리스트, ArrayList).java
+++ java/13. 경로탐색(인접리스트, ArrayList).java
@@ -3,11 +3,10 @@ import java.util.*;
 
 
 class Main {
-	static int n,m,answer=0;
+	static int n,m,answer;
 	static ArrayList<ArrayList<Integer>> graph;
 	static int [] ch;
-	
-	public void DFS(int v) {
+	public static void DFS(int v) {
 		if(v==n) answer++;
 		else {
 			for(int nv:graph.get(v)) {
@@ -18,27 +17,32 @@ class Main {
 				}
 			}
 		}
-
 	}
 	
 	public static void main(String[] args){
+		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
 		n=kb.nextInt();
 		m=kb.nextInt();
 		graph=new ArrayList<ArrayList<Integer>>();
+		
 		for(int i=0;i<=n;i++) {
 			graph.add(new ArrayList<Integer>());
 		}
-		ch=new int [n+1];
+		
+		ch=new int[n+1];
+		
 		for(int i=0;i<m;i++) {
 			int a=kb.nextInt();
 			int b=kb.nextInt();
 			graph.get(a).add(b);
 		}
+		
 		ch[1]=1;
 		T.DFS(1);
-
-		System.out.println(answer);
-	}
+		
+		System.out.print(answer);
+		
+}
 }
