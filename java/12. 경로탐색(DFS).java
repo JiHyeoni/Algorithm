6a687ab270cb7637ba1f625e373cd02f67acad12
Jihyeon <gihyeon0715@naver.com>
2021-08-23 10:22:27 +0900

    section7

--- java/12. 경로탐색(DFS).java
+++ java/12. 경로탐색(DFS).java
@@ -1,13 +1,14 @@
 import java.util.*;
 
-
-
 class Main {
-	static int n,m,answer=0;
-	static int[][] graph;
+	static int n,m;
+	static int [][] graph;
 	static int [] ch;
+	static int answer;
+
 	
 	public void DFS(int v) {
+		
 		if(v==n) answer++;
 		else {
 			for(int i=1;i<=n;i++) {
@@ -18,24 +19,28 @@ class Main {
 				}
 			}
 		}
-
 	}
 	
 	public static void main(String[] args){
+		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
+		
 		n=kb.nextInt();
 		m=kb.nextInt();
+		
 		graph=new int[n+1][n+1];
-		ch=new int [n+1];
+		ch=new int[n+1];
+		
 		for(int i=0;i<m;i++) {
 			int a=kb.nextInt();
-			int b=kb.nextInt();
+			int b=kb.nextInt();	
 			graph[a][b]=1;
 		}
+		
 		ch[1]=1;
 		T.DFS(1);
-
 		System.out.println(answer);
-	}
+		
+}
 }
