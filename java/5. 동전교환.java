48b6a2bfd97c86ab0fd206f5afd046453e2e4bdf
Jihyeon <gihyeon0715@naver.com>
2021-08-23 16:05:30 +0900

    section8

--- java/5. 동전교환.java
+++ java/5. 동전교환.java
@@ -1,34 +1,34 @@
 import java.util.*;
 
 class Main {
-	static int n;
-	static int m;
+	static int n,m;
+	static Integer [] coin;
 	static int min=Integer.MAX_VALUE;
-	public void DFS(int L,int sum,Integer[]coin) {
+	
+	public void DFS(int L,int sum) {
+		if(L>min) return;
 		if(sum>m) return;
-		if(L>=min) return;
-		if(sum==m) min=Math.min(min,L);
+		if(sum==m) min=Math.min(L, min);
 		else {
 			for(int i=0;i<n;i++) {
-				DFS(L+1,sum+coin[i],coin);
+				DFS(L+1,sum+coin[i]);
 			}
 		}
+		
 	}
-	
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
-		
 		n=kb.nextInt();
-		Integer [] coin=new Integer[n];
+		coin=new Integer[n];
 		for(int i=0;i<n;i++) {
 			coin[i]=kb.nextInt();
 		}
-		Arrays.sort(coin, Collections.reverseOrder());
 		m=kb.nextInt();
-		
-		T.DFS(0,0,coin);
+		Arrays.sort(coin,Collections.reverseOrder());
+
+		T.DFS(0,0);
 		System.out.print(min);
 		
 }
