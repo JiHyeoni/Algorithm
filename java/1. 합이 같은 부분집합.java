9548bcff20e791c3da3a7a875a217a7c83a49f59
Jihyeon <gihyeon0715@naver.com>
2021-08-25 23:54:36 +0900

    section8

--- java/1. 합이 같은 부분집합.java
+++ java/1. 합이 같은 부분집합.java
@@ -1,42 +1,41 @@
 import java.util.*;
 
-
-
 class Main {
-	static String answer="NO";
-	boolean flag=false;
 	static int n;
-	static int [] arr; 
+	static int [] arr;
 	static int total;
-	
+	static boolean flag=false;
+	static String answer="NO";
+
 	public void DFS(int L,int sum) {
 		if(flag) return;
 		if(sum>(total/2)) return;
 		if(L==n) {
 			if(sum==(total-sum)) {
-				answer="YES";
 				flag=true;
+				answer="YES";
 			}
 		}
 		else {
-			DFS(L+1,sum+arr[L]);
 			DFS(L+1,sum);
+			DFS(L+1,sum+arr[L]);
 		}
+
 	}
-	
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
 		n=kb.nextInt();
 		arr=new int[n];
-		total=0;
 		for(int i=0;i<n;i++) {
 			arr[i]=kb.nextInt();
 			total+=arr[i];
 		}
 		
-		T.DFS(0, 0);
-		System.out.print(answer);
+		T.DFS(0,0);
+		
+		System.out.println(answer);
+		
 }
 }
