efc56693eee462fcf822d0e7c547f335f2f29937
Jihyeon <gihyeon0715@naver.com>
2021-08-31 13:12:44 +0900

    section8

--- java/9. 조합 구하기.java
+++ java/9. 조합 구하기.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class Main {	
 	static int n,m;
-	static int p[];
+	static int [] p;
+	
 	public void DFS(int L, int s) {
 		if(L==m) {
 			for(int x:p) System.out.print(x+" ");
@@ -10,11 +11,12 @@ class Main {
 		}
 		else {
 			for(int i=s;i<=n;i++) {
-					p[L]=i;
-					DFS(L+1,i+1);
+				p[L]=i;
+				DFS(L+1,i+1);
 			}
 		}
 	}
+	
 	public static void main(String[] args){
 		
 		Main T = new Main();
@@ -22,7 +24,8 @@ class Main {
 		n=kb.nextInt();
 		m=kb.nextInt();
 		p=new int[m];
-		T.DFS(0,1);
 		
+		T.DFS(0,1);
+
 }
 }
