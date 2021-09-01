878a8641be1b8a86fc6081c3b0c3c4c61368f2f5
Jihyeon <gihyeon0715@naver.com>
2021-09-01 14:56:09 +0900

    section8

--- java/8. 수열 추측하기.java
+++ java/8. 수열 추측하기.java
@@ -1,22 +1,25 @@
 import java.util.*;
 
-class Main {
-	static int[] b,p,ch;
+
+
+class Main {	
 	static int n,f;
-	boolean flag=false;
-	int[][] dy=new int[35][35];
+	static int [] ch,p,b;
+	static int [][] dy=new int [11][11];
+	static boolean flag= false;
 	
 	public int combi(int n,int r) {
-		if(dy[n][r]>0) return dy[n][r];
-		if (n==r||r==0) return 1;
+		if(n==r||r==0) return 1;
+		if(dy[n][r]!=0) return dy[n][r];
 		else return dy[n][r]=combi(n-1,r-1)+combi(n-1,r);
 	}
-
+	
 	public void DFS(int L, int sum) {
+	
 		if(flag) return;
 		if(L==n) {
 			if(sum==f) {
-				for(int x:p) System.out.print(x+" ");
+				for(int x : p) System.out.print(x+" ");
 				flag=true;
 			}
 		}
@@ -25,28 +28,28 @@ class Main {
 				if(ch[i]==0) {
 					ch[i]=1;
 					p[L]=i;
-					DFS(L+1,sum+(p[L]*b[L]));
+					DFS(L+1,sum+p[L]*b[L]);
 					ch[i]=0;
 				}
 			}
 		}
-
-
 	}
+	
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
 		n=kb.nextInt();
 		f=kb.nextInt();
+		ch=new int [n+1];
+		p=new int [n];
 		b=new int[n];
-		p=new int[n];
-		ch=new int[n+1];
+		
 		for(int i=0;i<n;i++) {
-			b[i]=T.combi(n-1,i);
+			b[i]=T.combi(n-1, i);
 		}
 		
-		T.DFS(0,0);
-		
+		T.DFS(0, 0);
+
 }
 }
