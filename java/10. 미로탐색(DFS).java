0cb83270dbcc24a7dde60ae283b8a7285771fbdd
Jihyeon <gihyeon0715@naver.com>
2021-09-01 15:07:23 +0900

    section8

--- java/10. 미로탐색(DFS).java
+++ java/10. 미로탐색(DFS).java
@@ -1,38 +1,46 @@
 import java.util.*;
 
-class Main {	
 
-	int [] dx= {-1,0,1,0};
-	int [] dy= {0,-1,0,1};
-	static int[][] dis;
+
+class Main {	
+	static int [][] board;
 	static int answer;
-	public void DFS(int x, int y) {
-		if(x==6 && y==6) answer++;
+	static int [] dx= {0,-1,0,1};
+	static int [] dy= {-1,0,1,0};
+	
+	public void DFS(int x,int y) {
+	
+		if(x==7 && y==7) answer++;
 		else {
 			for(int i=0;i<4;i++) {
-				if(x+dx[i]>=0&&y+dy[i]>=0&&x+dx[i]<7&&y+dy[i]<7&&dis[x+dx[i]][y+dy[i]]==0) {
-					dis[x+dx[i]][y+dy[i]]=1;
-					DFS(x+dx[i],y+dy[i]);
-					dis[x+dx[i]][y+dy[i]]=0;
+				int nx=x+dx[i];
+				int ny=y+dy[i];
+				
+				if(nx>=1 && nx<=7 && ny>=1 && ny<=7 && board[nx][ny]==0) {
+					board[nx][ny]=1;
+					DFS(nx,ny);
+					board[nx][ny]=0;
 				}
 			}
 		}
-
+	
 	}
+	
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
-		dis=new int[7][7];
-		for(int i=0;i<7;i++) {
-			for(int j=0;j<7;j++) {
-				dis[i][j]=kb.nextInt();
+		
+		board=new int[8][8];
+		for(int i=1;i<8;i++) {
+			for(int j=1;j<8;j++) {
+				board[i][j]=kb.nextInt();
 			}
 		}
-		dis[0][0]=1;
-		T.DFS(0,0);
 		
+		board[1][1]=1;
+		T.DFS(1, 1);
+
 		System.out.print(answer);
-		
 }
 }
