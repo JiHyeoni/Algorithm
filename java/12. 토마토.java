e6dba4ba0f5e796a617dd705192081966dfb58a4
Jihyeon <gihyeon0715@naver.com>
2021-09-01 15:42:40 +0900

    section8

--- java/12. 토마토.java
+++ java/12. 토마토.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Point{
 	int x,y;
-	Point(int x, int y){
+	Point (int x, int y){
 		this.x=x;
 		this.y=y;
 	}
@@ -10,10 +10,9 @@ class Point{
 
 class Main {	
 	static int m,n;
-	static int[][] board, dis;
-	static int[] dx={0,-1,0,1};
-	static int[] dy= {-1,0,1,0};
-	static int answer;
+	static int [][] board,dis;
+	static int [] dx= {0,-1,0,1};
+	static int [] dy= {-1,0,1,0};
 	static Queue<Point> Q=new LinkedList<>();
 	
 	public void BFS() {
@@ -22,32 +21,36 @@ class Main {
 			for(int i=0;i<4;i++) {
 				int nx=tmp.x+dx[i];
 				int ny=tmp.y+dy[i];
-				if(nx>=0 && nx<n && ny>=0 && ny<m && dis[nx][ny]==0) {
-					board[nx][ny]=1;
+				if(nx>=0&&nx<n&&ny>=0&&ny<m&&board[nx][ny]==0) {
 					Q.offer(new Point(nx,ny));
+					board[nx][ny]=1;
 					dis[nx][ny]=dis[tmp.x][tmp.y]+1;
 				}
 			}
-		}		
+		}
 	}
 	
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
+		
 		m=kb.nextInt();
 		n=kb.nextInt();
 		board=new int[n][m];
 		dis=new int[n][m];
+		
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<m;j++) {
 				board[i][j]=kb.nextInt();
 				if(board[i][j]==1) Q.offer(new Point(i,j));
 			}
 		}
+		
 		T.BFS();
 		boolean flag=true;
 		int answer=Integer.MIN_VALUE;
+		
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<m;j++) {
 				if(board[i][j]==0) flag=false;
@@ -62,7 +65,7 @@ class Main {
 			}
 			System.out.print(answer);
 		}
-
 		else System.out.print(-1);
+		
 }
 }
