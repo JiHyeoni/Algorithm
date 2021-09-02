38d2e08dfad95e12d929bf8424b49e29e472d92e
Jihyeon <gihyeon0715@naver.com>
2021-09-02 13:28:56 +0900

    section8

---
+++ java/13. 섬나라 아일랜드(DFS).java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+
+class Main {	
+	static int n;
+	static int [][] board;
+	static int [] dx= {-1,1,-1,1,1,-1,0,0};
+	static int [] dy= {-1,1,1,-1,0,0,-1,1};
+	static int answer;
+	
+	public void DFS(int x,int y) {
+		for(int i=0;i<8;i++) {
+			int nx=x+dx[i];
+			int ny=y+dy[i];
+			if(nx>=0&&nx<n&&ny>=0&&ny<n&&board[nx][ny]==1) {
+				board[nx][ny]=0;
+				DFS(nx,ny);
+			}
+		}
+	}
+	
+	public void solution() {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(board[i][j]==1) {
+					board[i][j]=0;
+					DFS(i,j);
+					answer++;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		
+		Main T = new Main();
+		Scanner kb=new Scanner(System.in);
+		
+		n=kb.nextInt();
+		board=new int[n][n];
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				board[i][j]=kb.nextInt();
+			}
+		}
+		
+		T.solution();
+		
+		System.out.print(answer);
+		
+}
+}
