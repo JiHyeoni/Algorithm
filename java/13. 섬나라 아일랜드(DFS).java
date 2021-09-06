54a1cf061822c05f71a52f736d824960a6284db8
Jihyeon <gihyeon0715@naver.com>
2021-09-06 16:51:49 +0900

    section8

--- java/13. 섬나라 아일랜드(DFS).java
+++ java/13. 섬나라 아일랜드(DFS).java
@@ -2,18 +2,19 @@ import java.util.*;
 
 
 class Main {	
+	
 	static int n;
-	static int [][] board;
-	static int [] dx= {-1,1,-1,1,1,-1,0,0};
-	static int [] dy= {-1,1,1,-1,0,0,-1,1};
+	static int[][] arr;
 	static int answer;
+	static int [] dx= {-1,1,-1,1,0,1,0,-1};
+	static int [] dy= {-1,1,1,-1,-1,0,1,0};
 	
 	public void DFS(int x,int y) {
 		for(int i=0;i<8;i++) {
-			int nx=x+dx[i];
-			int ny=y+dy[i];
-			if(nx>=0&&nx<n&&ny>=0&&ny<n&&board[nx][ny]==1) {
-				board[nx][ny]=0;
+			int nx=dx[i]+x;
+			int ny=dy[i]+y;
+			if(nx>=0&&nx<n&&ny>=0&&ny<n&&arr[nx][ny]==1) {
+				arr[nx][ny]=0;
 				DFS(nx,ny);
 			}
 		}
@@ -22,26 +23,27 @@ class Main {
 	public void solution() {
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
-				if(board[i][j]==1) {
-					board[i][j]=0;
-					DFS(i,j);
+				if(arr[i][j]==1) {
+					arr[i][j]=0;
 					answer++;
+					DFS(i,j);
 				}
 			}
 		}
 	}
 	
+
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
 		
 		n=kb.nextInt();
-		board=new int[n][n];
+		arr=new int[n][n];
 		
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
-				board[i][j]=kb.nextInt();
+				arr[i][j]=kb.nextInt();
 			}
 		}
 		
