453cfa4886a8b4925a05c6243b43967f960b31b7
Jihyeon <gihyeon0715@naver.com>
2021-09-07 21:57:08 +0900

    section8

--- java/15. 피자배달거리.java
+++ java/15. 피자배달거리.java
@@ -1,17 +1,18 @@
 import java.util.*;
 
 class Point{
-	public int x,y;
-	Point(int x,int y){
+	int x,y;
+	
+	public Point(int x, int y) {
 		this.x=x;
 		this.y=y;
 	}
 }
 
 class Main {	
-	static int n, m, len, answer=Integer.MAX_VALUE;
-	static int []combi;
-	static ArrayList<Point> pz,hs;
+	static int n,m,len,answer=Integer.MAX_VALUE;
+	static int [] combi;
+	static ArrayList<Point> hs,pz;
 	
 	public void DFS(int L,int s) {
 		if(L==m) {
@@ -23,8 +24,7 @@ class Main {
 				}
 				sum+=dis;
 			}
-			answer=Math.min(answer, sum);
-			
+			answer=Math.min(sum, answer);
 		}
 		else {
 			for(int i=s;i<len;i++) {
@@ -32,7 +32,10 @@ class Main {
 				DFS(L+1,i+1);
 			}
 		}
+
+
 	}
+
 	
 
 	public static void main(String[] args){
@@ -42,8 +45,9 @@ class Main {
 		
 		n=kb.nextInt();
 		m=kb.nextInt();
-		pz=new ArrayList<>();
+		combi=new int[m];
 		hs=new ArrayList<>();
+		pz=new ArrayList<>();
 		
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
@@ -52,10 +56,9 @@ class Main {
 				else if(tmp==2) pz.add(new Point(i,j));
 			}
 		}
-		
 		len=pz.size();
-		combi=new int[m];
-		T.DFS(0,0);
+		T.DFS(0, 0);
 		System.out.print(answer);
+
 }
 }
