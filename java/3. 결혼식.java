dbaad09597971d064084f83dbdff7f41e99f1e18
Jihyeon <gihyeon0715@naver.com>
2021-09-09 10:49:01 +0900

    section9

--- java/3. 결혼식.java
+++ java/3. 결혼식.java
@@ -1,47 +1,51 @@
 import java.util.*;
 
 class Time implements Comparable<Time>{
-	public int time;
-	public char state;
-	Time(int time,char state){
+	int time;
+	char state;
+	Time(int time, char state){
 		this.time=time;
 		this.state=state;
 	}
 	
 	@Override
-	public int compareTo(Time ob) {
-		if(this.time==ob.time) return this.state-ob.state;
-		else return this.time-ob.time;
+	public int compareTo (Time o) {
+		if(this.time==o.time) return this.state-o.state;
+		else return this.time-o.time;
 	}
 }
+
+
 
 class Main {	
 	
 	public int solution(ArrayList<Time> arr) {
-		int answer=Integer.MIN_VALUE;
-		Collections.sort(arr);
 		int cnt=0;
+		int max=Integer.MIN_VALUE;
+		Collections.sort(arr);
 		for(Time ob:arr) {
 			if(ob.state=='s') cnt++;
-			else cnt--;
-			answer=Math.max(answer, cnt);
+			else if(ob.state=='e') cnt--;
+			max=Math.max(max, cnt);
 		}
-		return answer;
+		return max;
 	}
-
 
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
 		
-		int n=kb.nextInt();
 		ArrayList<Time> arr=new ArrayList<>();
+		int n=kb.nextInt();
+		
 		for(int i=0;i<n;i++) {
 			int sT=kb.nextInt();
 			int eT=kb.nextInt();
+			
 			arr.add(new Time(sT,'s'));
 			arr.add(new Time(eT,'e'));
+			
 		}
 		
 		System.out.print(T.solution(arr));
