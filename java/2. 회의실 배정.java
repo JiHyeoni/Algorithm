2fcf8ee41c79ae23053ba7b3dd29aa5d20cc1ded
Jihyeon <gihyeon0715@naver.com>
2021-09-09 10:38:58 +0900

    section9

--- java/2. 회의실 배정.java
+++ java/2. 회의실 배정.java
@@ -1,51 +1,53 @@
 import java.util.*;
 
 class Time implements Comparable<Time>{
-	int s;
-	int e;
-	Time(int s,int e){
+	int s,e;
+	Time(int s, int e){
 		this.s=s;
 		this.e=e;
 	}
 	
 	@Override
-	public int compareTo(Time t) {
-		if(this.e==t.e) return this.s-t.s;
-		else return this.e-t.e;
+	public int compareTo (Time o) {
+		if(this.e==o.e) return this.s-o.s;
+		else return this.e-o.e;
 	}
 	
 }
 
+
+
 class Main {	
 	
-	public int solution(ArrayList<Time> arr, int n) {
+	public int solution(ArrayList<Time> arr) {
 		int cnt=0;
 		int et=0;
 		Collections.sort(arr);
-		for(Time t:arr) {
-			if(t.s>=et) {
+		for(Time ob:arr) {
+			if(ob.s>=et) {
+				//System.out.println(ob.s+" "+ob.e);
 				cnt++;
-				et=t.e;
+				et=ob.e;
 			}
 		}
 		return cnt;
 	}
-
 
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
 		
-		int n=kb.nextInt();
 		ArrayList<Time> arr=new ArrayList<>();
+		int n=kb.nextInt();
+		
 		for(int i=0;i<n;i++) {
 			int s=kb.nextInt();
 			int e=kb.nextInt();
 			arr.add(new Time(s,e));
 		}
 		
-		System.out.print(T.solution(arr, n));
+		System.out.print(T.solution(arr));
 
 }
 }
