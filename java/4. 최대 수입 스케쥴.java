5452be5ead0effdd261534f4393f422d5cd5061f
Jihyeon <gihyeon0715@naver.com>
2021-09-10 11:58:02 +0900

    section9

--- java/4. 최대 수입 스케쥴.java
+++ java/4. 최대 수입 스케쥴.java
@@ -1,39 +1,35 @@
 import java.util.*;
 
 class Lecture implements Comparable<Lecture>{
-	public int m;
-	public int d;
-	Lecture(int m,int d){
+	int m,d;
+	Lecture(int m, int d){
 		this.m=m;
 		this.d=d;
 	}
-	
+
 	@Override
-	public int compareTo(Lecture ob) {
-		return ob.d-this.d;
+	public int compareTo(Lecture o) {
+		return o.d-this.d;
 	}
 }
 
 class Main {	
 	static int n,max=Integer.MIN_VALUE;
-	
 	public int solution(ArrayList<Lecture> arr) {
-		int answer=0;
-		PriorityQueue<Integer> pQ=new PriorityQueue<>(Collections.reverseOrder());
 		Collections.sort(arr);
+		PriorityQueue<Integer> pQ=new PriorityQueue<>(Collections.reverseOrder());
+		int sum=0;
 		int j=0;
-		for(int i=max;i>=1;i--) {
+		for(int i=max;i>0;i--) {
 			for(;j<n;j++) {
-				if(arr.get(j).d<i) break;
+				if(i>arr.get(j).d) break;
 				pQ.offer(arr.get(j).m);
 			}
-			if(!pQ.isEmpty()) answer+=pQ.poll();
+			if(!pQ.isEmpty()) sum+=pQ.poll();
 		}
-
-		return answer;
+		
+		return sum;
 	}
-
-
 	public static void main(String[] args){
 		
 		Main T = new Main();
@@ -46,8 +42,8 @@ class Main {
 			int d=kb.nextInt();
 			arr.add(new Lecture(m,d));
 			if(d>max) max=d;
-		}
+			}
+		
 		System.out.print(T.solution(arr));
-
-}
+		}
 }
