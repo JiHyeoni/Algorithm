45ae1887483f7e0fa4fce1bac6c885c390ef9b94
Jihyeon <gihyeon0715@naver.com>
2021-09-13 17:13:59 +0900

    section9

--- java/1. 씨름선수.java
+++ java/1. 씨름선수.java
@@ -1,50 +1,52 @@
 import java.util.*;
 
 class Body implements Comparable<Body>{
-	int h, w;
-	Body(int h,int w){
-		this.h=h;
-		this.w=w;
+	
+	int height,weight;
+	Body(int height,int weight){
+		this.weight=weight;
+		this.height=height;
 	}
 	
 	@Override
 	public int compareTo(Body o) {
-		if(o.h==this.h) return o.w-this.w;
-		else return o.h-this.h;
+		if(o.height==this.height) return o.weight-this.weight;
+		else return o.height-this.height;
 	}
 }
 
 
 class Main {	
+
 	
 	public int solution(ArrayList<Body> arr) {
 		int cnt=0;
-		int max=Integer.MIN_VALUE;
 		Collections.sort(arr);
+		int max=Integer.MIN_VALUE;
+		
 		for(Body ob:arr) {
-			if(ob.w>max) {
-				max=ob.w;
+			if(ob.weight>max) {
+				max=ob.weight;
 				cnt++;
 			}
 		}
+		
 		return cnt;
 	}
-
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
 		
-		ArrayList<Body> arr=new ArrayList<>();
 		int n=kb.nextInt();
-		
+		ArrayList<Body> arr= new ArrayList<>();
 		for(int i=0;i<n;i++) {
-			int h=kb.nextInt();
-			int w=kb.nextInt();
-			arr.add(new Body(h,w));
+			int height=kb.nextInt();
+			int weight=kb.nextInt();
+			arr.add(new Body(height,weight));
 		}
 		
 		System.out.print(T.solution(arr));
-
-}
+		
+	}
 }
