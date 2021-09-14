12ed5e65373a696fbba6a855e5343b0e60606abe
Jihyeon <gihyeon0715@naver.com>
2021-09-14 10:22:22 +0900

    section9

--- java/5. 다익스트라 알고리즘.java
+++ java/5. 다익스트라 알고리즘.java
@@ -1,42 +1,48 @@
 import java.util.*;
 
-class Edge implements Comparable<Edge>{
-	public int vex;
-	public int cost;
-	Edge(int vex, int cost){
+class Point implements Comparable<Point>{
+	int vex;
+	int cost;
+	Point(int vex,int cost){
 		this.vex=vex;
 		this.cost=cost;
 	}
 	
 	@Override
-	public int compareTo(Edge ob) {
-		return this.cost-ob.cost;
+	public int compareTo(Point o) {
+		return this.cost-o.cost;
 	}
 }
 
-
 class Main {	
 	static int n,m;
-	static ArrayList<ArrayList<Edge>> graph;
 	static int [] dis;
-
+	static ArrayList<ArrayList<Point>> graph;
 	public void solution(int v) {
-		PriorityQueue<Edge> pQ=new PriorityQueue<>();
-		pQ.offer(new Edge(v,0));
+		PriorityQueue<Point> pQ=new PriorityQueue<>();
+		pQ.add(new Point(v,0));
 		dis[v]=0;
+		
 		while(!pQ.isEmpty()) {
-			Edge tmp=pQ.poll();
+			Point tmp=pQ.poll();
 			int now=tmp.vex;
 			int nowCost=tmp.cost;
-			if(nowCost>dis[now]) continue;
-			for(Edge ob:graph.get(now)) {
+			if(dis[now]<nowCost) continue;
+			for(Point ob:graph.get(now)) {
 				if(dis[ob.vex]>nowCost+ob.cost) {
 					dis[ob.vex]=nowCost+ob.cost;
-					pQ.offer(new Edge(ob.vex,nowCost+ob.cost));
+					pQ.add(new Point(ob.vex,nowCost+ob.cost));
 				}
 			}
 		}
+		
+		for(int i=2;i<=n;i++) {
+			if(dis[i]==Integer.MAX_VALUE) System.out.println(i+" : impossible");
+			else System.out.println(i+" : " + dis[i]);
+		}
+
 	}
+	
 	public static void main(String[] args){
 		
 		Main T = new Main();
@@ -44,23 +50,22 @@ class Main {
 		
 		n=kb.nextInt();
 		m=kb.nextInt();
-		graph=new ArrayList<ArrayList<Edge>>();
-		for(int i=0;i<=n;i++) {
-			graph.add(new ArrayList<Edge>());
-		}
+		graph=new ArrayList<ArrayList<Point>>();
 		dis=new int[n+1];
-		Arrays.fill(dis, Integer.MAX_VALUE);
+		for(int i=0;i<=n;i++) {
+			graph.add(new ArrayList<>());
+		}
+		
 		for(int i=0;i<m;i++) {
 			int a=kb.nextInt();
 			int b=kb.nextInt();
 			int c=kb.nextInt();
-			graph.get(a).add(new Edge(b,c));
+			graph.get(a).add(new Point(b,c));
 		}
 		
+		Arrays.fill(dis, Integer.MAX_VALUE);
+		
 		T.solution(1);
-		for(int i=2;i<=n;i++) {
-			if(dis[i]!=Integer.MAX_VALUE) System.out.println(i+" : "+dis[i]);
-			else System.out.println(i+" : imposible");
-		}
-}
+		
+	}
 }
