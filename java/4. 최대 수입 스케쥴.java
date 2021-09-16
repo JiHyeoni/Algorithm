45d9611d5e7c9c6f358ba6e84bf2722ed06304f2
Jihyeon <gihyeon0715@naver.com>
2021-09-16 20:12:40 +0900

    section9

--- java/4. 최대 수입 스케쥴.java
+++ java/4. 최대 수입 스케쥴.java
@@ -1,35 +1,35 @@
 import java.util.*;
 
 class Lecture implements Comparable<Lecture>{
-	int m,d;
-	Lecture(int m, int d){
+	int m;
+	int d;
+	Lecture(int m,int d){
 		this.m=m;
 		this.d=d;
 	}
-
+	
 	@Override
 	public int compareTo(Lecture o) {
 		return o.d-this.d;
 	}
 }
-
 class Main {	
 	static int n,max=Integer.MIN_VALUE;
 	public int solution(ArrayList<Lecture> arr) {
-		Collections.sort(arr);
 		PriorityQueue<Integer> pQ=new PriorityQueue<>(Collections.reverseOrder());
+		Collections.sort(arr);
 		int sum=0;
 		int j=0;
-		for(int i=max;i>0;i--) {
+		for(int i=max;i>=1;i--) {
 			for(;j<n;j++) {
 				if(i>arr.get(j).d) break;
-				pQ.offer(arr.get(j).m);
+				pQ.add(arr.get(j).m);
 			}
 			if(!pQ.isEmpty()) sum+=pQ.poll();
 		}
-		
 		return sum;
 	}
+
 	public static void main(String[] args){
 		
 		Main T = new Main();
@@ -37,13 +37,15 @@ class Main {
 		ArrayList<Lecture> arr=new ArrayList<>();
 		
 		n=kb.nextInt();
+		
 		for(int i=0;i<n;i++) {
 			int m=kb.nextInt();
 			int d=kb.nextInt();
 			arr.add(new Lecture(m,d));
-			if(d>max) max=d;
-			}
+			if(max<d) max=d;
+		}
 		
 		System.out.print(T.solution(arr));
-		}
+		
+	}
 }
