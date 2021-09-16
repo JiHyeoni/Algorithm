116e4cc3b6f8e3e915dad849257ca77014ae5838
Jihyeon <gihyeon0715@naver.com>
2021-09-16 20:35:15 +0900

    section9

--- java/5. 다익스트라 알고리즘.java
+++ java/5. 다익스트라 알고리즘.java
@@ -1,71 +1,72 @@
 import java.util.*;
 
-class Point implements Comparable<Point>{
+class Edge implements Comparable<Edge>{
+	
 	int vex;
 	int cost;
-	Point(int vex,int cost){
+	Edge(int vex, int cost){
 		this.vex=vex;
 		this.cost=cost;
 	}
 	
 	@Override
-	public int compareTo(Point o) {
+	public int compareTo(Edge o) {
 		return this.cost-o.cost;
 	}
 }
-
 class Main {	
 	static int n,m;
+	static ArrayList<ArrayList<Edge>> graph;
 	static int [] dis;
-	static ArrayList<ArrayList<Point>> graph;
+	
 	public void solution(int v) {
-		PriorityQueue<Point> pQ=new PriorityQueue<>();
-		pQ.add(new Point(v,0));
+		PriorityQueue<Edge> pQ=new PriorityQueue<>();
 		dis[v]=0;
+		pQ.offer(new Edge(v,0));
 		
 		while(!pQ.isEmpty()) {
-			Point tmp=pQ.poll();
+			Edge tmp=pQ.poll();
 			int now=tmp.vex;
 			int nowCost=tmp.cost;
 			if(dis[now]<nowCost) continue;
-			for(Point ob:graph.get(now)) {
+			for(Edge ob:graph.get(now)) {
 				if(dis[ob.vex]>nowCost+ob.cost) {
 					dis[ob.vex]=nowCost+ob.cost;
-					pQ.add(new Point(ob.vex,nowCost+ob.cost));
+					pQ.offer(new Edge(ob.vex,nowCost+ob.cost));
 				}
 			}
 		}
-		
-		for(int i=2;i<=n;i++) {
-			if(dis[i]==Integer.MAX_VALUE) System.out.println(i+" : impossible");
-			else System.out.println(i+" : " + dis[i]);
-		}
 
 	}
-	
+
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
-		
 		n=kb.nextInt();
 		m=kb.nextInt();
-		graph=new ArrayList<ArrayList<Point>>();
+		graph=new ArrayList<ArrayList<Edge>>();
 		dis=new int[n+1];
+		
 		for(int i=0;i<=n;i++) {
-			graph.add(new ArrayList<>());
+			graph.add(new ArrayList<Edge>());
 		}
 		
 		for(int i=0;i<m;i++) {
 			int a=kb.nextInt();
 			int b=kb.nextInt();
 			int c=kb.nextInt();
-			graph.get(a).add(new Point(b,c));
+			graph.get(a).add(new Edge(b,c));
 		}
 		
 		Arrays.fill(dis, Integer.MAX_VALUE);
 		
 		T.solution(1);
+		
+		for(int i=2;i<=n;i++) {
+			if(dis[i]==Integer.MAX_VALUE) System.out.println(i+" : impossible");
+			else System.out.println(i +" : " + dis[i]);
+		}
 		
 	}
 }
