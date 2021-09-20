4a744f2163c7ace82a2f4ce5add59831a4e417b7
Jihyeon <gihyeon0715@naver.com>
2021-09-21 00:00:12 +0900

    section9

--- java/5. 다익스트라 알고리즘.java
+++ java/5. 다익스트라 알고리즘.java
@@ -9,8 +9,8 @@ class Edge implements Comparable<Edge>{
 	}
 	
 	@Override
-	public int compareTo(Edge ob) {
-		return this.cost-ob.cost;
+	public int compareTo(Edge o) {
+		return this.cost-o.cost;
 	}
 }
 
@@ -19,36 +19,39 @@ class Main {
 	static int [] dis;
 	static ArrayList<ArrayList<Edge>> graph;
 
-	public void solution(int v) {
+	public static void solution(int v) {
 		PriorityQueue<Edge> pQ=new PriorityQueue<>();
 		dis[v]=0;
 		pQ.offer(new Edge(v,0));
-		
 		while(!pQ.isEmpty()) {
 			Edge tmp=pQ.poll();
 			int now=tmp.vex;
 			int nowCost=tmp.cost;
+			
 			if(dis[now]<nowCost) continue;
-			for(Edge o:graph.get(now)) {
-				if(dis[o.vex]>nowCost+o.cost) {
-					dis[o.vex]=nowCost+o.cost;
-					pQ.offer(new Edge(o.vex,nowCost+o.cost));
+			for(Edge ob:graph.get(now)) {
+				if(dis[ob.vex]>nowCost+ob.cost) {
+					dis[ob.vex]=nowCost+ob.cost;
+					pQ.offer(new Edge(ob.vex,nowCost+ob.cost));
 				}
 			}
 		}
+		
+		for(int i=2;i<=n;i++) {
+			if(dis[i]==Integer.MAX_VALUE) System.out.println(i+" : impossible");
+			else System.out.println(i+" : "+dis[i]);
+		}
 	}
-
+	
 	public static void main(String[] args){
 		
 		Main T = new Main();
 		Scanner kb=new Scanner(System.in);
-		
 		n=kb.nextInt();
 		m=kb.nextInt();
 		dis=new int[n+1];
 		Arrays.fill(dis, Integer.MAX_VALUE);
 		graph=new ArrayList<ArrayList<Edge>>();
-		
 		for(int i=0;i<=n;i++) {
 			graph.add(new ArrayList<Edge>());
 		}
@@ -61,11 +64,8 @@ class Main {
 			graph.get(a).add(new Edge(b,c));
 		}
 		
-		T.solution(1);
+		solution(1);
 		
-		for(int i=2;i<=n;i++) {
-			if(dis[i]==Integer.MAX_VALUE) System.out.println(i+" : impossible");
-			else System.out.println(i+" : "+dis[i]);
-		}
+
 	}
 }
