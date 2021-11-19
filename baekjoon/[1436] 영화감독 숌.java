08f4dcf92ca5b02ebfda84e3a7d7feb7b89f8c9a
Jihyeon <gihyeon0715@naver.com>
2021-11-19 22:56:31 +0900

    브루트 포스

--- baekjoon/[1436] 영화감독 숌.java
+++ baekjoon/[1436] 영화감독 숌.java
@@ -1,14 +1,16 @@
 import java.util.*;
-
 class Main{
     public static void main(String [] args){
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        int count=1;
+        int cnt=1;
         int num=666;
-        while(count!=n){
+
+        while(cnt!=n){
             num++;
-            if(String.valueOf(num).contains("666")) count++;
+            if(String.valueOf(num).contains("666")){
+                cnt++;
+            }
         }
 
         System.out.println(num);
