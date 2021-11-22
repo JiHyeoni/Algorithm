14dd30caa83875b0de669b67df7b1926a485dca4
Jihyeon <gihyeon0715@naver.com>
2021-11-22 23:10:45 +0900

    재귀

--- baekjoon/[11729] 하노이 탑 이동 순서.java
+++ baekjoon/[11729] 하노이 탑 이동 순서.java
@@ -2,27 +2,23 @@ import java.util.*;
 
 class Main{
     public static StringBuilder sb=new StringBuilder();
-
-    public static void main(String [] args){
+    public static void main(String []args){
         Scanner s=new Scanner(System.in);
+
         int k=s.nextInt();
-
-        sb.append((int)(Math.pow(2,k)-1)).append('\n');
-
+        sb.append((int)Math.pow(2,k)-1);
+        sb.append("\n");
         Hanoi(k,1,2,3);
         System.out.println(sb);
     }
 
-    public static void Hanoi(int num, int from, int mid, int to){
-        if(num==1){
+    public static void Hanoi(int k, int from, int by, int to){
+        if(k==1){
             sb.append(from+" "+to+"\n");
             return;
         }
-        else{
-            Hanoi(num-1,from,to,mid);
-            sb.append(from+" "+to+"\n");
-            Hanoi(num-1,mid,from,to);
-        }
-
+        Hanoi(k-1,from,to,by);
+        sb.append(from+" "+to+"\n");
+        Hanoi(k-1,by,from,to);
     }
 }
