bb74cee157c15cf3e4770a7cd5354ff548940ae5
Jihyeon <gihyeon0715@naver.com>
2021-11-22 23:40:27 +0900

    기본 수학 2

--- baekjoon/[9020] 골드바흐의 추측.java
+++ baekjoon/[9020] 골드바흐의 추측.java
@@ -1,10 +1,10 @@
 import java.util.*;
-
 class Main{
+    static int arr[];
     public static void main(String [] args){
         Scanner s=new Scanner(System.in);
 
-        int [] arr=new int [10001];
+        arr= new int [10001];
         arr[1]=1;
         for(int i=2;i<=10000;i++){
             if(arr[i]==0){
@@ -16,67 +16,27 @@ class Main{
             }
         }
 
-        int t=s.nextInt();
-        for(int i=0;i<t;i++){
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
             int n=s.nextInt();
-            int j=n/2;
-            int k=n/2;
-            while(true){
-                if(arr[j]==0&&arr[k]==0){
-                    if(n==j+k){
-                        System.out.println(j+" "+k);
-                        break;
-                    }
-                }
-                j--;
-                k++;
-            }
+            sosu(n);
         }
     }
-}
 
-/*
-import java.util.*;
+    public static void sosu(int n){
+        int i=n/2;
+        int j=n/2;
 
-class Main{
-    public static void main(String [] args){
-        Scanner s=new Scanner(System.in);
-
-        int [] arr=new int [10001];
-        arr[1]=1;
-        for(int i=2;i<=10000;i++){
-            if(arr[i]==0){
-                int j=2;
-                while(i*j<=10000){
-                    arr[i*j]=1;
-                    j++;
+        while(true){
+            if(i+j==n){
+                if(arr[i]==0&&arr[j]==0){
+                    break;
                 }
             }
+            i--;
+            j++;
         }
 
-        int t=s.nextInt();
-        for(int i=0;i<t;i++){
-            int n=s.nextInt();
-            int min=Integer.MAX_VALUE;
-            int a=0;
-            int b=0;
-            for(int j=2;j<=10000;j++){
-                if(arr[j]==0){
-                    for(int k=2;k<=10000;k++){
-                        if(arr[k]==0){
-                            if(j+k==n){
-                                if(k-j>=0&&min>k-j){
-                                    min=k-j;
-                                    a=j;
-                                    b=k;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            System.out.println(a+" "+b);
-        }
+        System.out.println(i+" "+j);
     }
 }
- */
