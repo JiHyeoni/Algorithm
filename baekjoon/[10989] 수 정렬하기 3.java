6e667f17910119653a282aae49ad4ab28b7d31bf
Jihyeon <gihyeon0715@naver.com>
2021-11-24 20:58:18 +0900

    정렬

---
+++ baekjoon/[10989] 수 정렬하기 3.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String [] args) throws IOException{
+        int [] cnt=new int[10001];
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+
+        for(int i=0;i<N;i++){
+            cnt[Integer.parseInt(br.readLine())]++;
+        }
+
+        br.close();
+
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<=10000;i++){
+            if(cnt[i]>0){
+                while(cnt[i]!=0){
+                    sb.append(i).append("\n");
+                    cnt[i]--;
+                }
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+}
