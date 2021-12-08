73d48f02aa2eef984fc394492de6014e5d02e03b
Jihyeon <gihyeon0715@naver.com>
2021-12-08 19:04:25 +0900

    동적 계획법 1

---
+++ baekjoon/[1904] 01타일.java
@@ -0,0 +1,34 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+class Main{
+    static int arr[];
+    public static void main(String [] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int N=Integer.parseInt(br.readLine());
+        arr=new int[N+1];
+
+       System.out.println(find(N));
+    }
+
+
+    public static int find(int n){
+        if(n<3) {
+            return n;
+        }
+        else{
+            arr[0]=0;
+            arr[1]=1;
+            arr[2]=2;
+
+            for(int i=3;i<=n;i++){
+                arr[i]=(arr[i-2]+arr[i-1])%15746;
+            }
+            return arr[n];
+        }
+    }
+
+
+}
