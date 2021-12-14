188847945de49f4821894980f71167a1cdc07a4c
Jihyeon <gihyeon0715@naver.com>
2021-12-14 17:34:57 +0900

    정렬

--- baekjoon/[18870] 좌표 압축.java
+++ baekjoon/[18870] 좌표 압축.java
@@ -1,36 +1,35 @@
-import java.util.*;
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.*;
 
 class Main{
     public static void main(String [] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int N=Integer.parseInt(br.readLine());
-        StringTokenizer st=new StringTokenizer(br.readLine());;
-        int arr[] = new int[N];
-        int arrClone[];
-        Map<Integer,Integer> map=new HashMap<>();
+        int [] arr=new int[N];
+        int [] arrClone;
+        StringTokenizer st=new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
-            int num=Integer.parseInt(st.nextToken());
-            arr[i]=num;
+            arr[i]=Integer.parseInt(st.nextToken());
         }
+
         arrClone=arr.clone();
         Arrays.sort(arrClone);
 
+        Map<Integer,Integer> map=new HashMap<>();
         int count=0;
-        for(int i=0;i<arrClone.length;i++){
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0;i<N;i++){
             if(!map.containsKey(arrClone[i])){
                 map.put(arrClone[i],count++);
             }
         }
 
-        StringBuilder sb=new StringBuilder();
-
-        for(int i=0;i<N;i++){
-            sb.append(map.get(arr[i])+" ");
+        for(int x : arr){
+            sb.append(map.get(x)+" ");
         }
-
         System.out.println(sb);
     }
 }
