2a8b5c503d86b0e1d7947954f6f273c0237644da
Jihyeon <gihyeon0715@naver.com>
2021-12-15 21:57:34 +0900

    동적 계획법 1

--- baekjoon/[9461] 파도반 수열.java
+++ baekjoon/[9461] 파도반 수열.java
@@ -1,45 +1,24 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
 
 class Main{
-    static long p[];
-    static StringBuilder sb=new StringBuilder();
-    public static void main(String [] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int T=Integer.parseInt(br.readLine());
-        p=new long[101];
-        p[1]=1L;
-        p[2]=1L;
-        p[3]=1L;
-        p[4]=2L;
-        p[5]=2L;
-        p[6]=3L;
-        p[7]=4L;
-        p[8]=5L;
-        p[9]=7L;
-        p[10]=9L;
+    static long [] arr;
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        arr=new long[101];
+        int T=s.nextInt();
         for(int i=0;i<T;i++){
-            int N=Integer.parseInt(br.readLine());
-            triangle(N);
+            int N=s.nextInt();
+            System.out.println(P(N));
         }
-        System.out.println(sb);
     }
 
-    public static void triangle(int N){
-        if(p[N]!=0)
-        {
-            sb.append(p[N]).append("\n");
-            return;
+    public static long P(int N){
+        arr[1]=1l;
+        arr[2]=1l;
+        arr[3]=1l;
+        for(int i=4;i<=N;i++){
+            if(arr[i]==0) arr[i]=arr[i-2]+arr[i-3];
         }
-        else{
-            for(int i=11;i<=N;i++){
-                if(p[i]==0){
-                    p[i]=p[i-5]+p[i-1];
-                }
-            }
-            sb.append(p[N]).append("\n");
-        }
-
+        return arr[N];
     }
 }
