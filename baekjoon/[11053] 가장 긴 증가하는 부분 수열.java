ea94bdaeed3ce796eb5be24c1aa5f4f695b7411b
Jihyeon <gihyeon0715@naver.com>
2021-12-22 21:38:30 +0900

    동적 계획법 1

--- baekjoon/[11053] 가장 긴 증가하는 부분 수열.java
+++ baekjoon/[11053] 가장 긴 증가하는 부분 수열.java
@@ -1,36 +1,39 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Main{
+    static int [] arr;
+    static int [] dp;
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        arr=new int[n+1];
+        dp=new int[n+1];
+        for(int i=1;i<=n;i++) arr[i]=s.nextInt();
 
-    public static void main(String [] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        int [] arr=new int[n+1];
-        int [] dp=new int[n+1];
-        StringTokenizer st=new StringTokenizer(br.readLine()," ");
         for(int i=1;i<=n;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
-        }
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            int val=0;
-            for(int j=1;j<=i;j++){
-                if(arr[j]<arr[i]){
-                    if(val<dp[j]) val=dp[j];
-                }
-            }
-            dp[i]=val+1;
+            sequence(i);
         }
 
-        int max=0;
-        for(int x:dp) {
-            //System.out.print(x+" ");
-            max=Math.max(x,max);
+        int max=Integer.MIN_VALUE;
+        for (int i=1;i<=n;i++) {
+            max=Math.max(max,dp[i]);
+            //System.out.print(dp[i]+" ");
         }
+        //System.out.println();
         System.out.println(max);
 
+
+    }
+
+    public static int sequence(int n){
+        if(dp[n]==0){
+            dp[n]=1;
+            for(int i=1;i<=n;i++){
+                if(arr[n]>arr[i]){
+                    dp[n]=Math.max(dp[n],sequence(i)+1);
+                }
+            }
+        }
+        return dp[n];
     }
 }
