aa0f5674fef6df8dc3e8d11164df31e6cacc0bf9
Jihyeon <gihyeon0715@naver.com>
2021-12-23 17:10:36 +0900

    동적 계획법 1

--- baekjoon/[1912] 연속합.java
+++ baekjoon/[1912] 연속합.java
@@ -1,30 +1,39 @@
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
 class Main{
-    public static void main(String [] args){
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
-        int [] arr=new int[n];
-        int [] sum=new int[n];
-
+    static int n;
+    static int arr[];
+    static Integer dp[];
+    public static void main(String [] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        n=Integer.parseInt(br.readLine());
+        arr=new int[n];
+        dp=new Integer[n];
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
         for(int i=0;i<n;i++){
-            arr[i]=s.nextInt();
+            arr[i]=Integer.parseInt(st.nextToken());
         }
-
-        sum[0]=arr[0];
-        for(int i=1;i<n;i++){
-            if(sum[i-1]>0){
-                sum[i]=sum[i-1]+arr[i];
-            }
-            else {
-                sum[i]=arr[i];
-            }
-        }
+        sum(n-1);
 
         int max=Integer.MIN_VALUE;
-        for (int x : sum) {
-            max=Math.max(x,max);
+        for(int i=0;i<n;i++){
+            max=Math.max(max,dp[i]);
         }
-
         System.out.println(max);
+    }
+
+    public static int sum(int k){
+        if(k==0){
+            return dp[0]=arr[0];
+        }
+        else {
+            if (dp[k] == null) {
+                dp[k] = Math.max(arr[k], sum(k - 1) + arr[k]);
+            }
+            return dp[k];
+        }
     }
 }
