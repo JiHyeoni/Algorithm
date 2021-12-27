454c62ed7248383d7b6f7cd764b8000d49e859c3
Jihyeon <gihyeon0715@naver.com>
2021-12-28 01:23:06 +0900

    동적 계획법 1

--- baekjoon/[10844] 쉬운 계단 수.java
+++ baekjoon/[10844] 쉬운 계단 수.java
@@ -1,28 +1,31 @@
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
 
 class Main{
+    static int N;
+    static long [][] stair;
     public static void main(String [] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int N=Integer.parseInt(br.readLine());
-        long sum=0;
-        long [][] arr=new long[N+1][10];
+        N=Integer.parseInt(br.readLine());
+        stair=new long[N][10];
 
-        for(int i=1;i<10;i++){
-            arr[1][i]=1l;
+        for(int i=0;i<10;i++){
+            stair[0][i]=1l;
         }
 
-        for(int i=2;i<=N;i++){
+        for(int i=1;i<N;i++){
             for(int j=0;j<10;j++){
-                if(j==0) arr[i][j]=arr[i-1][j+1]%1000000000;
-                else if(j==9) arr[i][j]=arr[i-1][j-1]%1000000000;
-                else arr[i][j]=(arr[i-1][j-1]+arr[i-1][j+1])%1000000000;
+                if(j==0) stair[i][j]=stair[i-1][j+1]%1000000000;
+                else if(j==9) stair[i][j]=stair[i-1][j-1]%1000000000;
+                else stair[i][j]=(stair[i-1][j-1]+stair[i-1][j+1])%1000000000;
             }
         }
 
-        for(int i=0;i<10;i++){
-            sum+=arr[N][i];
+        long sum=0;
+        for(int i=1;i<10;i++){
+            sum+=stair[N-1][i];
         }
 
         System.out.println(sum%1000000000);
