44bac2bbcb658d9074a4a64ebf741362146fba25
Jihyeon <gihyeon0715@naver.com>
2022-01-07 22:12:00 +0900

    스택

--- baekjoon/[4949] 균형잡힌 세상.java
+++ baekjoon/[4949] 균형잡힌 세상.java
@@ -1,43 +1,43 @@
+import java.util.*;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.Stack;
-
+import java.io.InputStreamReader;
 class Main{
     public static void main(String [] args) throws IOException{
+        StringBuilder sb=new StringBuilder();
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       while(true){
+           String str=br.readLine();
+           if(str.equals(".")) break;
+           Stack<Character> stack=new Stack<>();
+           boolean check=true;
+           for(int i=0;i<str.length();i++){
+               if(str.charAt(i)=='('||str.charAt(i)=='[') stack.add(str.charAt(i));
+               else if(str.charAt(i)==')'){
+                   if (stack.isEmpty()||stack.peek()=='['){
+                       check=false;
+                       break;
+                   }
+                   else stack.pop();
+               }
+               else if(str.charAt(i)==']'){
+                   if (stack.isEmpty()||stack.peek()=='('){
+                       check=false;
+                       break;
+                   }
+                   else stack.pop();
+               }
+           }
 
-        while(true){
-            String str=br.readLine();
-            if(str.equals(".")){
-                break;
-            }
-            System.out.println(world(str));
-        }
+           if(check==false||!stack.isEmpty()){
+               sb.append("no").append("\n");
+           }
+           else{
+               sb.append("yes").append("\n");
+           }
+       }
 
-    }
-
-    private static String world(String str) {
-        Stack<Character> stack=new Stack<>();
-
-        for(int i = 0; i< str.length(); i++){
-            if(str.charAt(i)=='('|| str.charAt(i)=='[') stack.push(str.charAt(i));
-            else if(str.charAt(i)==')'){
-                if (stack.isEmpty()|| stack.peek()!='('){
-                    return "no";
-                }
-                else stack.pop();
-            }
-            else if(str.charAt(i)==']'){
-                if(stack.isEmpty()|| stack.peek()!='['){
-                    return "no";
-                }
-                else stack.pop();
-            }
-        }
-
-        if(!stack.isEmpty()) return "no";
-        else return "yes";
+        System.out.println(sb);
 
     }
 }
