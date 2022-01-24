7be932c95a69f3eb476c95151edd402053801d16
Jihyeon <gihyeon0715@naver.com>
2022-01-24 22:45:21 +0900

    동적 계획법 1

--- baekjoon/[2156] 포도주 시식.java
+++ baekjoon/[2156] 포도주 시식.java
@@ -3,27 +3,31 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 class Main{
-    static int n;
     static int [] juice;
-    static Integer [] sum;
+    static Integer [] drink;
+    static int n;
     public static void main(String [] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         n=Integer.parseInt(br.readLine());
         juice=new int[n+1];
-        sum=new Integer[n+1];
-        for(int i=1;i<=n;i++){
+        drink=new Integer[n+1];
+        for(int i=1;i<n+1;i++){
             juice[i]=Integer.parseInt(br.readLine());
         }
-        System.out.println(drink(n));
-    }
+        System.out.println(grape(n));
 
-    public static int drink(int k){
-        if(k==0) return sum[k]=0;
-        if(k==1) return sum[k]=juice[k];
-        if(k==2) return sum[k]=juice[k-1]+juice[k];
-        if(sum[k]==null){
-            sum[k]=Math.max(Math.max(drink(k-2),drink(k-3)+juice[k-1])+juice[k],drink(k-1));
+    }
+    public static int grape(int N){
+        if (N==0) return drink[0]=0;
+        if(N==1){
+            return drink[1]=juice[1];
         }
-        return sum[k];
+        if(N==2){
+            return drink[2]=juice[1]+juice[2];
+        }
+        if(drink[N]==null){
+            drink[N]=Math.max(Math.max(grape(N-2),grape(N-3)+juice[N-1])+juice[N],grape(N-1));
+        }
+        return drink[N];
     }
 }
