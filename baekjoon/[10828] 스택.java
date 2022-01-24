cb8e382f51badbba47e367ef598617ccb8a943c4
Jihyeon <gihyeon0715@naver.com>
2022-01-24 21:50:15 +0900

    스택

--- baekjoon/[10828] 스택.java
+++ baekjoon/[10828] 스택.java
@@ -1,37 +1,49 @@
+import java.io.IOException;
 import java.util.*;
 
 class Main{
-    public static void main(String [] args){
+    public static void main(String [] args) throws IOException{
         Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
         int [] stack=new int[10001];
-        int size=0;
+        int point=0;
+        int n=s.nextInt();
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<n;i++){
             String str=s.next();
             if(str.equals("push")){
                 int number=s.nextInt();
-                size++;
-                stack[size]=number;
-            }
-            else if(str.equals("size")){
-                sb.append(size).append("\n");
-            }
-            else if (str.equals("empty")){
-                if(size==0) sb.append(1).append("\n");
-                else sb.append(0).append("\n");
+                stack[point]=number;
+                point++;
             }
             else if(str.equals("pop")){
-                if(size==0) sb.append(-1).append("\n");
-                else sb.append(stack[size--]).append("\n");
+                if(point==0){
+                    sb.append(-1).append("\n");
+                }
+                else{
+                    sb.append(stack[--point]).append("\n");
+                }
+            }
+            else if(str.equals("size")){
+                sb.append(point).append("\n");
+            }
+            else if(str.equals("empty")){
+                if(point==0){
+                    sb.append(1).append("\n");
+                }
+                else{
+                    sb.append(0).append("\n");
+                }
             }
             else if(str.equals("top")){
-                if(size==0) sb.append(-1).append("\n");
-                else sb.append(stack[size]).append("\n");
+                if(point==0){
+                    sb.append(-1).append("\n");
+                }
+                else{
+                    sb.append(stack[point-1]).append("\n");
+                }
             }
         }
+
         System.out.println(sb);
-
-
     }
 }
