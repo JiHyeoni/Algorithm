90a4a77129f997a6b126b6e247dfe52e42866d6d
Jihyeon <gihyeon0715@naver.com>
2022-01-24 21:38:54 +0900

    그리디 알고리즘

--- baekjoon/[13305] 주유소.java
+++ baekjoon/[13305] 주유소.java
@@ -1,4 +1,3 @@
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
@@ -8,28 +7,27 @@ class Main{
     public static void main(String [] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-
-        long distance[]=new long[n-1];
-        long coin[] =new long[n];
+        long [] distance=new long[n-1];
+        long [] price=new long[n];
         StringTokenizer st=new StringTokenizer(br.readLine()," ");
         for(int i=0;i<n-1;i++){
             distance[i]=Long.parseLong(st.nextToken());
         }
-
-        st=new StringTokenizer(br.readLine());
+        st=new StringTokenizer(br.readLine()," ");
         for(int i=0;i<n;i++){
-            coin[i]=Long.parseLong(st.nextToken());
+            price[i]=Long.parseLong(st.nextToken());
+        }
+        long money=distance[0]*price[0];
+        for(int i=1;i<n-1;i++){
+            if(price[i-1]<price[i]){
+                price[i]=price[i-1];
+            }
+            money+=price[i]*distance[i];
         }
 
-        for(int i=1;i<n;i++){
-            if(coin[i-1]<coin[i]) coin[i]=coin[i-1];
-        }
+        System.out.println(money);
 
-        long sum=0l;
-        for(int i=0;i<n-1;i++){
-            sum+=coin[i]*distance[i];
-        }
 
-        System.out.println(sum);
+
     }
 }
