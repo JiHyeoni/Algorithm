9e6d104da2de1bb6c7c7250bcfc1ba830dd429d4
Jihyeon <gihyeon0715@naver.com>
2022-01-25 16:06:25 +0900

    분할 정복

--- baekjoon/[2630] 색종이 만들기.java
+++ baekjoon/[2630] 색종이 만들기.java
@@ -1,47 +1,46 @@
-import java.util.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.StringTokenizer;
 
 
-class Main{
-    static int white=0;
-    static int blue=0;
-    static int [][] arr;
-    public static void main(String [] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
+class Main {
+    static int n;
+    static int arr[][];
+    static int blue;
+    static int white;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n=Integer.parseInt(br.readLine());
         arr=new int[n][n];
-        StringTokenizer st;
         for(int i=0;i<n;i++){
-            st=new StringTokenizer(br.readLine()," ");
+            StringTokenizer st=new StringTokenizer(br.readLine()," ");
             for(int j=0;j<n;j++){
                 arr[i][j]=Integer.parseInt(st.nextToken());
             }
         }
-
-        quadTree(0,0,n);
+        color(0,0,n);
         System.out.println(white);
         System.out.println(blue);
     }
-    public static void quadTree(int row, int col, int size){
-        if(checkColor(row,col,size)){
+    public static void color(int row,int col,int size){
+        if(check(row,col,size)){
             if(arr[row][col]==0) white++;
             else blue++;
             return;
         }
         size=size/2;
-        quadTree(row,col,size);
-        quadTree(row+size,col,size);
-        quadTree(row,col+size,size);
-        quadTree(row+size,col+size,size);
+        color(row,col,size);
+        color(row,col+size,size);
+        color(row+size,col,size);
+        color(row+size,col+size,size);
     }
 
-    public static boolean checkColor(int row,int col,int size){
-        int color=arr[row][col];
+    public static boolean check(int row, int col, int size){
+        int first=arr[row][col];
         for(int i=row;i<row+size;i++){
             for(int j=col;j<col+size;j++){
-                if(arr[i][j]!=color) return false;
+                if(first!=arr[i][j]) return false;
             }
         }
         return true;
