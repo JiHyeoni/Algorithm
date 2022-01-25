d77b84d388bd7a37d15a99d89e720def0b8ccee6
Jihyeon <gihyeon0715@naver.com>
2022-01-25 15:36:20 +0900

    덱

--- baekjoon/[1021] 회전하는 큐.java
+++ baekjoon/[1021] 회전하는 큐.java
@@ -1,39 +1,59 @@
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
-class Main{
-    public static void main(String [] args){
-        Scanner s=new Scanner(System.in);
+
+class Main {
+    static int count=0;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
         LinkedList<Integer> deque=new LinkedList<>();
-        int n=s.nextInt();
-        int m=s.nextInt();
+        n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
         int [] arr=new int[m];
-        for(int i=0;i<m;i++){
-            arr[i]=s.nextInt();
-        }
         for(int i=1;i<=n;i++){
-            deque.offer(i);
+            deque.offerLast(i);
         }
-        int count=0;
-
+        st=new StringTokenizer(br.readLine()," ");
         for(int i=0;i<m;i++){
-            int target_idx=deque.indexOf(arr[i]);
-            int half_idx=deque.size()/2;
-            if(target_idx<=half_idx){
-                for(int j=0;j<target_idx;j++){
-                    deque.offerLast(deque.pollFirst());
-                    count++;
-                }
-            }
-            else{
-                for(int j=0;j<deque.size()-target_idx;j++){
-                    deque.offerFirst(deque.pollLast());
-                    count++;
-                }
-            }
-            deque.pollFirst();
+            arr[i]=Integer.parseInt(st.nextToken());
         }
 
+        for(int i=0;i<m;i++) {
+            rotation(deque,arr[i]);
+        }
         System.out.println(count);
+    }
+    public static void rotation(LinkedList<Integer> deque,int k){
+        if(deque.peekFirst()==k){
+            deque.pollFirst();
+            return;
+        }
+        int half=deque.size()/2;
+        int target=0;
+        for(int i=0;i<deque.size();i++){
+            if(deque.get(i)==k) target=i;
+        }
+
+        if(target>half){
+            while(!deque.isEmpty()&&deque.peekFirst()!=k){
+                deque.offerFirst(deque.pollLast());
+                count++;
+            }
+        }
+        else{
+            while(!deque.isEmpty()&&deque.peekFirst()!=k){
+                deque.offerLast(deque.pollFirst());
+                count++;
+            }
+        }
+        deque.pollFirst();
+        return;
 
     }
+
 }
