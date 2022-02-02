eee8964cb5f0758c70375a85ab088a9e3d4c90f0
Jihyeon <gihyeon0715@naver.com>
2022-02-02 21:43:44 +0900

    분할 정복

--- baekjoon/[2630] 색종이 만들기.java
+++ baekjoon/[2630] 색종이 만들기.java
@@ -3,40 +3,41 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
 
-
-class Main {
-    static int n;
-    static int arr[][];
+class Main{
+    static int [][] arr;
     static int blue;
     static int white;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n=Integer.parseInt(br.readLine());
+    public static void main(String [] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        StringTokenizer st;
         arr=new int[n][n];
         for(int i=0;i<n;i++){
-            StringTokenizer st=new StringTokenizer(br.readLine()," ");
+            st=new StringTokenizer(br.readLine()," ");
             for(int j=0;j<n;j++){
                 arr[i][j]=Integer.parseInt(st.nextToken());
             }
         }
-        color(0,0,n);
+        paper(0,0,n);
         System.out.println(white);
         System.out.println(blue);
     }
-    public static void color(int row,int col,int size){
+
+    public static void paper(int row,int col,int size){
         if(check(row,col,size)){
             if(arr[row][col]==0) white++;
             else blue++;
             return;
         }
+
         size=size/2;
-        color(row,col,size);
-        color(row,col+size,size);
-        color(row+size,col,size);
-        color(row+size,col+size,size);
+        paper(row,col,size);
+        paper(row,col+size,size);
+        paper(row+size,col,size);
+        paper(row+size,col+size,size);
     }
 
-    public static boolean check(int row, int col, int size){
+    public static boolean check(int row,int col,int size){
         int first=arr[row][col];
         for(int i=row;i<row+size;i++){
             for(int j=col;j<col+size;j++){
@@ -46,3 +47,4 @@ class Main {
         return true;
     }
 }
+
