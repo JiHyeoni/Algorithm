38268a6949a38d65ba08d71ca47ae5e6372dd2a4
Jihyeon <gihyeon0715@naver.com>
2022-02-14 11:09:26 +0900

    DFS와 BFS

--- baekjoon/[1260] DFS와 BFS.java
+++ baekjoon/[1260] DFS와 BFS.java
@@ -1,67 +1,72 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Main{
     static int [][] arr;
-    static boolean[] check;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String args[]) throws IOException{
+    static int [] check;
+    static ArrayList<Integer> result=new ArrayList<>();
+    public static void main(String [] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        StringTokenizer st=new StringTokenizer(br.readLine(), " ");
         int n=Integer.parseInt(st.nextToken());
-        int m=Integer.parseInt(st.nextToken());
+        int m= Integer.parseInt(st.nextToken());
         int v=Integer.parseInt(st.nextToken());
-        arr=new int [n+1][n+1];
-        check=new boolean[n+1];
+        arr=new int[n+1][n+1];
+        check=new int[n+1];
         for(int i=0;i<m;i++){
             st=new StringTokenizer(br.readLine()," ");
-            int start=Integer.parseInt(st.nextToken());
-            int end=Integer.parseInt(st.nextToken());
-            arr[start][end]=1;
-            arr[end][start]=1;
+            int s=Integer.parseInt(st.nextToken());
+            int e=Integer.parseInt(st.nextToken());
+            arr[s][e]=1;
+            arr[e][s]=1;
         }
+        StringBuilder sb=new StringBuilder();
         dfs(v);
+        for (Integer integer : result) {
+            sb.append(integer).append(" ");
+        }
         sb.append("\n");
         bfs(v);
+        for (Integer integer : result) {
+            sb.append(integer).append(" ");
+        }
         System.out.println(sb);
+
     }
 
-    public static void initCheck(){
+    public static void init(){
+        result.clear();
         for(int i=0;i<check.length;i++){
-            check[i]=false;
+            check[i]=0;
         }
     }
-
     public static void dfs(int v){
-            sb.append(v).append(" ");
-            check[v]=true;
-            for(int i=1;i<arr.length;i++){
-                if(i!=v&&arr[v][i]==1&&check[i]==false){
-                    dfs(i);
-                }
+        check[v]=1;
+        result.add(v);
+        for(int i=1;i<check.length;i++){
+            if(v!=i&&arr[v][i]==1&&check[i]==0){
+                dfs(i);
             }
+        }
     }
 
     public static void bfs(int v){
-        initCheck();
+        init();
         Queue<Integer> queue=new LinkedList<>();
         queue.add(v);
-        check[v]=true;
+        check[v]=1;
+        result.add(v);
         while(!queue.isEmpty()){
-            int k=queue.poll();
-            sb.append(k).append(" ");
-            for(int i=1;i<arr.length;i++){
-                if(check[i]==false&&i!=k&&arr[k][i]==1){
+            int now=queue.poll();
+            for(int i=1;i<check.length;i++){
+                if(now!=i&&arr[now][i]==1&&check[i]==0){
                     queue.add(i);
-                    check[i]=true;
+                    check[i]=1;
+                    result.add(i);
                 }
             }
-
         }
-
     }
 }
