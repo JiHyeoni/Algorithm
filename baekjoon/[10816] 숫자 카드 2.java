de19257cbc805c7d57f5608a99fe570d3c46ffc1
Jihyeon <gihyeon0715@naver.com>
2022-02-15 17:03:06 +0900

    이분 탐색

--- baekjoon/[10816] 숫자 카드 2.java
+++ baekjoon/[10816] 숫자 카드 2.java
@@ -1,62 +1,54 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Main{
-    static int [] a;
-    static int [] x;
+    static int [] A;
     public static void main(String [] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-        a=new int[n];
-        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        A=new int[n];
+        StringTokenizer st=new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
-            a[i]=Integer.parseInt(st.nextToken());
+            A[i]=Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(a);
         int m=Integer.parseInt(br.readLine());
-        x=new int [m];
-        st=new StringTokenizer(br.readLine()," ");
+        int [] B=new int [m];
+        st=new StringTokenizer(br.readLine());
         for(int i=0;i<m;i++){
-            x[i]=Integer.parseInt(st.nextToken());
+            B[i]=Integer.parseInt(st.nextToken());
         }
-
+        Arrays.sort(A);
         StringBuilder sb=new StringBuilder();
-        for (int k : x) {
-            sb.append(upper(k)-lower(k)).append(" ");
+        for(int num:B){
+            sb.append(up(num)-down(num)).append(" ");
         }
         System.out.println(sb);
     }
 
-    public static int upper(int k){
+    public static int up(int num){
         int left=0;
-        int right=a.length;
+        int right=A.length;
         while(left<right){
             int mid=(left+right)/2;
-            if(a[mid]<=k){
-                left=mid+1;
-            }
-            else{
-                right=mid;
-            }
+            if(A[mid]<=num) left=mid+1;
+            else right=mid;
         }
         return left;
     }
-    public static int lower(int k){
+
+    public static int down(int num){
+
         int left=0;
-        int right=a.length;
+        int right=A.length;
         while(left<right){
             int mid=(left+right)/2;
-            if(a[mid]>=k){
-                right=mid;
-            }
-            else{
-                left=mid+1;
-            }
+            if(A[mid]>=num) right=mid;
+            else left=mid+1;
         }
         return right;
     }
-}
 
+
+}
