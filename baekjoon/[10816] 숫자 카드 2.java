80f6a381855d4f120b6b7431f0b5003f6cc95aee
Jihyeon <gihyeon0715@naver.com>
2022-02-22 17:07:58 +0900

    이분 탐색

--- baekjoon/[10816] 숫자 카드 2.java
+++ baekjoon/[10816] 숫자 카드 2.java
@@ -1,54 +1,55 @@
+import java.util.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.*;
 
 class Main{
     static int [] A;
     public static void main(String [] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
         A=new int[n];
-        StringTokenizer st=new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
             A[i]=Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(A);
+
         int m=Integer.parseInt(br.readLine());
-        int [] B=new int [m];
+        int [] B=new int[m];
         st=new StringTokenizer(br.readLine());
         for(int i=0;i<m;i++){
             B[i]=Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(A);
+
         StringBuilder sb=new StringBuilder();
-        for(int num:B){
-            sb.append(up(num)-down(num)).append(" ");
+        for(int x:B){
+            sb.append(up(x)-down(x)).append(" ");
         }
         System.out.println(sb);
     }
 
-    public static int up(int num){
+    public static int up(int x){
         int left=0;
         int right=A.length;
         while(left<right){
             int mid=(left+right)/2;
-            if(A[mid]<=num) left=mid+1;
-            else right=mid;
+            if(A[mid]>x) right=mid;
+            else left=mid+1;
         }
         return left;
     }
 
-    public static int down(int num){
-
+    public static int down(int x){
         int left=0;
         int right=A.length;
+
         while(left<right){
             int mid=(left+right)/2;
-            if(A[mid]>=num) right=mid;
+            if(A[mid]>=x) right=mid;
             else left=mid+1;
         }
         return right;
     }
-
-
 }
