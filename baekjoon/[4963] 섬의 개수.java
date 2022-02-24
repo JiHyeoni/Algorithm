a929a2b27f3c4e8f786ac1c40f55dc9c2cda853e
Jihyeon <gihyeon0715@naver.com>
2022-02-24 21:28:11 +0900

    DFS와 BFS

--- baekjoon/[4963] 섬의 개수.java
+++ baekjoon/[4963] 섬의 개수.java
@@ -1,3 +1,88 @@
+//BFS
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+class Point{
+    int x;
+    int y;
+    Point(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+}
+
+class Main{
+    static int w;
+    static int h;
+    static int map[][];
+    static int check[][];
+    static int x[]={-1,0,1,0,-1,-1,1,1};
+    static int y[]={0,-1,0,1,-1,1,-1,1};
+    public static void main(String [] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb=new StringBuilder();
+        while(true){
+            st=new StringTokenizer(br.readLine()," ");
+            w=Integer.parseInt(st.nextToken());
+            h=Integer.parseInt(st.nextToken());
+
+            if(w==0&&h==0) break;
+
+            map=new int[h][w];
+            check=new int[h][w];
+            for(int i=0;i<h;i++){
+                st=new StringTokenizer(br.readLine()," ");
+                for(int j=0;j<w;j++){
+                    map[i][j]=Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int count=0;
+            for(int i=0;i<h;i++){
+                for(int j=0;j<w;j++){
+                    if(map[i][j]==1&&check[i][j]==0) {
+                        bfs(i, j);
+                        count++;
+                    }
+                }
+            }
+
+            sb.append(count).append("\n");
+
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void bfs(int v1,int v2) {
+        Queue<Point> queue=new LinkedList<>();
+        check[v1][v2]=1;
+        queue.add(new Point(v1,v2));
+
+        while(!queue.isEmpty()){
+            Point now=queue.poll();
+            for(int i=0;i<8;i++){
+                int nx=now.x+x[i];
+                int ny=now.y+y[i];
+                if(nx>=0&&ny>=0&&nx<h&&ny<w&&map[nx][ny]==1&&check[nx][ny]==0){
+                    check[nx][ny]=1;
+                    queue.add(new Point(nx,ny));
+                }
+            }
+        }
+    }
+}
+
+
+
+//DFS
+/*
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
@@ -56,3 +141,5 @@ class Main{
         }
     }
 }
+
+*/
