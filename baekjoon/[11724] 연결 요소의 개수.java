60e59fb806e94f20639b97e401199ec3a94cd77c
Jihyeon <gihyeon0715@naver.com>
2022-02-24 15:09:10 +0900

    DFS와 BFS

--- baekjoon/[11724] 연결 요소의 개수.java
+++ baekjoon/[11724] 연결 요소의 개수.java
@@ -1,3 +1,64 @@
+//BFS
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main{
+    static int n;
+    static int [][] arr;
+    static int [] check;
+    public static void main(String [] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+        arr=new int[n][n];
+        check=new int[n];
+
+        for(int i=0;i<m;i++){
+            st=new StringTokenizer(br.readLine());
+            int u=Integer.parseInt(st.nextToken());
+            int v=Integer.parseInt(st.nextToken());
+            arr[u-1][v-1]=1;
+            arr[v-1][u-1]=1;
+        }
+
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            if(check[i]==0){
+                bfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static void bfs(int x){
+        Queue<Integer> queue=new LinkedList<>();
+        queue.add(x);
+        while(!queue.isEmpty()){
+            Integer now=queue.poll();
+            for(int i=0;i<n;i++){
+                if(arr[now][i]==1&&check[i]==0){
+                    check[i]=1;
+                    queue.add(i);
+                }
+            }
+        }
+
+    }
+}
+
+
+
+
+
+//DFS
+/*
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -41,3 +102,4 @@ class Main{
         }
     }
 }
+*/
