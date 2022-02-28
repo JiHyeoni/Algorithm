a8c15aebd6b77b1655859b6ad6ecc5fe6faa7fc1
Jihyeon <gihyeon0715@naver.com>
2022-02-28 11:16:28 +0900

    그리디 알고리즘

--- baekjoon/[2217] 로프.java
+++ baekjoon/[2217] 로프.java
@@ -7,16 +7,16 @@ class Main{
     public static void main(String [] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-        int [] loop=new int[n];
+        int [] rope=new int[n];
         for(int i=0;i<n;i++){
-            loop[i]=Integer.parseInt(br.readLine());
+            rope[i]=Integer.parseInt(br.readLine());
         }
-        Arrays.sort(loop);
+        Arrays.sort(rope);
         int maxWeight=Integer.MIN_VALUE;
         int count=n;
 
         for(int i=0;i<n;i++){
-            maxWeight=Math.max(maxWeight,loop[i]*count);
+            maxWeight=Math.max(maxWeight,rope[i]*count);
             count--;
         }
 
