b0c1d95c8675b74ff10fd3ee33acaa2ebd0b1db8
Jihyeon <gihyeon0715@naver.com>
2022-03-08 16:00:21 +0900

    update '정수 제곱근 판별'

---
+++ programmers/level 1/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+
+        long number=(long)Math.sqrt(n);
+
+        if((long)Math.pow(number,2)==n){
+            answer=(long)Math.pow(number+1,2);
+        }
+
+        return answer;
+    }
+}
