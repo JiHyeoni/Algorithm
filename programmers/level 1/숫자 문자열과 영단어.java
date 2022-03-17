4bc71261729838db0f8670fa4146109ec127c3e6
Jihyeon <gihyeon0715@naver.com>
2022-03-17 14:32:01 +0900

    update '숫자 문자열과 영단어'

---
+++ programmers/level 1/숫자 문자열과 영단어.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String [] numbers={"0","1","2","3","4","5","6","7","8","9"};
+        String [] words={"zero","one","two","three","four","five","six","seven","eight","nine"};
+        for(int i=0;i<numbers.length;i++){
+            s=s.replaceAll(words[i],numbers[i]);
+        }
+        answer=Integer.parseInt(s);
+        return answer;
+    }
+}
